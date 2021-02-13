be7e1a50a34b243f022f26df378f20b3aedf334b
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-13 14:18:32 -0800

    Implemented react-router-dom and created routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import logo from './logo.svg';
 // import './App.css';
 
 import ProductsPage from './Components/ProductsPage';
+import ProductDetailsPage from './Components/ProductDetailsPage';
 
 import ProductProvider from './providers/ProductProvider';
 
 function App() {
   return (
-    <ProductProvider>
-      <ProductsPage />
-    </ProductProvider>
+    <Router>
+      <ProductProvider>
+        <Switch>
+          <Route exact path="/" render={() => <div>Homepage</div>} />
+          <Route path="/products" component={ProductsPage} />
+          <Route path="/products/:id" component={ProductDetailsPage} />
+        </Switch>
+      </ProductProvider>
+    </Router>
   );
 }
 
