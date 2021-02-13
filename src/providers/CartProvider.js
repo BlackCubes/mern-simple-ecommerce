dc88607a865a22ad71eea2edb5bb28ad32be22df
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-13 15:47:50 -0800

    Created a cart provider with context

---
+++ src/providers/CartProvider.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import { CartContext } from '../context';
+
+const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addProduct = (newProduct) => setCart((items) => [...items, newProduct]);
+
+  const removeProduct = (removeId) =>
+    setCart(cart.filter((obj) => obj.id !== removeId));
+
+  return (
+    <CartContext.Provider value={{ cart, addProduct, removeProduct }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+CartProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default CartProvider;
