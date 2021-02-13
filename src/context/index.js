eb81255bb6c36062f6fbf3d813a1a67bd2900658
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-12 19:28:33 -0800

    Imported and exported the contexts

---
+++ src/context/index.js
@@ -0,0 +1,9 @@
+import CartContext, { useCartContext } from './CartContext';
+import ProductContext, { useProductContext } from './ProductContext';
+
+export default {
+  CartContext,
+  useCartContext,
+  ProductContext,
+  useProductContext,
+};
