6a63f742da66868533882c661c04e1e4d68ec9bd
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-12 21:05:21 -0800

    Created a product provider functionality with api and context

---
+++ src/providers/ProductProvider.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+
+import ProductContext from '../context';
+
+import getProductsAPI from '../utils';
+
+const ProductProvider = ({ children }) => {
+  const [products, setProducts] = useState(null);
+
+  const getProducts = async () => {
+    try {
+      const data = await getProductsAPI();
+      setProducts(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <ProductContext.Provider value={products}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+ProductProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default ProductProvider;
