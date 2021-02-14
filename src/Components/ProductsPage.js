b1b75322edac68227207e5890064c9c3880104b5
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-14 12:19:51 -0800

    Refactored the prop in image component into object

--- src/Components/ProductsPage.js
+++ src/Components/ProductsPage.js
@@ -37,7 +37,7 @@ const ProductsPage = () => {
 
             <div className="product__image">
               <Link to={`/products/${prop.id}`}>
-                <Image src={prop.image} alt={prop.title} />
+                <Image rest={{ src: prop.image, alt: prop.title }} />
                 {/* <img src={prop.image} alt={prop.title} /> */}
               </Link>
             </div>
