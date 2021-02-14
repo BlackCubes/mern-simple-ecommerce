be94da87c23f9ab2e1e13cd9e10bb303b3ce69a6
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-14 11:26:55 -0800

    Imported and export module

--- src/common/index.js
+++ src/common/index.js
@@ -1,5 +1,6 @@
 import GlobalStyles from './GlobalStyles';
 import HeadingPrimary from './HeadingPrimary';
+import HeadingQuaternary from './HeadingQuaternary';
 import HeadingSecondary from './HeadingSecondary';
 
-export { GlobalStyles, HeadingPrimary, HeadingSecondary };
+export { GlobalStyles, HeadingPrimary, HeadingQuaternary, HeadingSecondary };
