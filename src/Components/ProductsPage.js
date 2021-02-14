73a5d23d1beb64ddd94909ca2246ed742253d0bd
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-13 17:04:58 -0800

    Imported cart context function with onClick event

--- src/Components/ProductsPage.js
+++ src/Components/ProductsPage.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { useCartContext } from '../context/CartContext';
 import { useProductContext } from '../context/ProductContext';
 
 const ProductsPage = () => {
   const { products } = useProductContext();
+  const { addProduct } = useCartContext();
 
   return (
     <div className="product--wrapper">
       {!products ? (
         <div>Hello!</div>
       ) : (
-        products.map((prop) => (
-          <div key={prop.id} className="product">
+        products.map((prop, key) => (
+          <div
+            key={prop.id}
+            className="product"
+            onClick={() => addProduct(products[key])}
+            onKeyDown={() => addProduct(products[key])}
+            role="presentation"
+          >
             <div className="product__title">
               <Link to={`/products/${prop.id}`}>{prop.title}</Link>
             </div>
