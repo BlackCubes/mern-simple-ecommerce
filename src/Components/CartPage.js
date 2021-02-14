f1c85a192f62a7f9738a83cde23efb666c4461fd
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-13 16:54:02 -0800

    Created a cart page component with context

---
+++ src/Components/CartPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { useCartContext } from '../context/CartContext';
+
+const CartPage = () => {
+  const { cart } = useCartContext();
+
+  return (
+    <div className="cart">
+      {!cart ? (
+        <div className="cart__empty">Empty</div>
+      ) : (
+        cart.map((prop) => <pre>{JSON.stringify(prop, null, 2)}</pre>)
+      )}
+    </div>
+  );
+};
+
+export default CartPage;
