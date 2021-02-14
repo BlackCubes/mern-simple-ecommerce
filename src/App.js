56d57177fdac075c1a2fc04f60bc24f148a0ad6c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-13 17:22:39 -0800

    Imported and implemented the navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import './App.css';
 
 import CartPage from './Components/CartPage';
+import Navbar from './Components/Navbar';
 import ProductsPage from './Components/ProductsPage';
 import ProductDetailsPage from './Components/ProductDetailsPage';
 
@@ -13,6 +14,7 @@ import ProductProvider from './providers/ProductProvider';
 function App() {
   return (
     <Router>
+      <Navbar />
       <ProductProvider>
         <CartProvider>
           <Switch>
