daae21825433b9985992bc5608decd752312e749
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-14 14:27:37 -0800

    Created multiple products styled components

---
+++ src/common/PricingStyled.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Products = styled.div`
+  width: 75%;
+`;
+
+export const ProductsCard = styled.div`
+  min-height: 23.5rem;
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+`;
+
+export const ProductsCardImage = styled.div`
+  width: 16%;
+`;
+
+export const ProductsCardRightColumn = styled.div`
+  width: 84%;
+`;
+
+export const ProductsCardInfo = styled.div`
+  width: 60%;
+`;
+
+export const ProductsCardPriceCart = styled.div`
+  width: 40%;
+`;
