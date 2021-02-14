345eaafc5b4c23031a68caaac2dfd7b53a4b262a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-13 22:25:09 -0800

    Imported and exported checkout context

--- src/context/index.js
+++ src/context/index.js
@@ -1,4 +1,12 @@
 import CartContext, { useCartContext } from './CartContext';
+import CheckoutContext, { useCheckoutContext } from './CheckoutContext';
 import ProductContext, { useProductContext } from './ProductContext';
 
-export { CartContext, useCartContext, ProductContext, useProductContext };
+export {
+  CartContext,
+  useCartContext,
+  CheckoutContext,
+  useCheckoutContext,
+  ProductContext,
+  useProductContext,
+};
