f511147b72ab0149793e765cc20aaf87d47d686d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-14 15:31:01 -0800

    Created a button styled component

---
+++ src/common/Button.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const ButtonStyled = styled.button`
+  display: inline-block;
+  width: 90%;
+  font-size: 1.6rem;
+  background-color: #ff1e5e;
+  padding: 1.5rem 4rem;
+  border: none;
+  border-radius: 1rem;
+  cursor: pointer;
+`;
+
+const Button = ({ children, rest }) => (
+  <ButtonStyled {...rest}>{children}</ButtonStyled>
+);
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  rest: PropTypes.shape({
+    as: PropTypes.string,
+    onClick: PropTypes.func,
+    onKeyDown: PropTypes.func,
+    role: PropTypes.string,
+  }),
+};
+
+Button.defaultProps = { rest: null };
+
+export default Button;
