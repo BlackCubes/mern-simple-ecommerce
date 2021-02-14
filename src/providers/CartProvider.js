37b6e8b2559466055ef8bf5898d073832a132fba
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-13 20:08:30 -0800

    Created function to increment cart quantity, and refactored

--- src/providers/CartProvider.js
+++ src/providers/CartProvider.js
@@ -7,7 +7,29 @@ const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
   console.log('cart: ', cart);
 
-  const addProduct = (newProduct) => setCart((items) => [...items, newProduct]);
+  const cartQuantity = (items, newItem) => {
+    if (!items.length) return [{ ...newItem, quantity: 1 }];
+
+    let quantity = 0;
+    let itemsObj = {};
+
+    items.forEach((item) => {
+      if (item.id !== newItem.id) {
+        quantity = 1;
+        itemsObj = { ...newItem, quantity };
+        items.push(itemsObj);
+      } else if (item.id === newItem.id) {
+        quantity += 1;
+        itemsObj = { ...item, quantity: item.quantity + quantity };
+        Object.assign(item, itemsObj);
+      }
+    });
+
+    return items;
+  };
+
+  const addProduct = (newProduct) =>
+    setCart((items) => cartQuantity(items, newProduct));
 
   const removeProduct = (removeId) =>
     setCart(cart.filter((obj) => obj.id !== removeId));
