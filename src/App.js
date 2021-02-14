74d3df5b13a8edabf29dc3805e11fe70945be259
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-13 16:56:56 -0800

    Imported and created a route for cart page

--- src/App.js
+++ src/App.js
@@ -3,20 +3,25 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import logo from './logo.svg';
 // import './App.css';
 
+import CartPage from './Components/CartPage';
 import ProductsPage from './Components/ProductsPage';
 import ProductDetailsPage from './Components/ProductDetailsPage';
 
+import CartProvider from './providers/CartProvider';
 import ProductProvider from './providers/ProductProvider';
 
 function App() {
   return (
     <Router>
       <ProductProvider>
-        <Switch>
-          <Route exact path="/" render={() => <div>Homepage</div>} />
-          <Route exact path="/products" component={ProductsPage} />
-          <Route exact path="/products/:id" component={ProductDetailsPage} />
-        </Switch>
+        <CartProvider>
+          <Switch>
+            <Route exact path="/" render={() => <div>Homepage</div>} />
+            <Route exact path="/products" component={ProductsPage} />
+            <Route exact path="/products/:id" component={ProductDetailsPage} />
+            <Route exact path="/cart" component={CartPage} />
+          </Switch>
+        </CartProvider>
       </ProductProvider>
     </Router>
   );
