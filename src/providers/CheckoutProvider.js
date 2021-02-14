3483281e47b7e587cef22875e515bd7c2b5708a4
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-13 23:00:45 -0800

    Created a checkout provider with context and functionalities

---
+++ src/providers/CheckoutProvider.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import { CheckoutContext } from '../context';
+
+const CheckoutProvider = ({ children }) => {
+  const [subTotal, setSubTotal] = useState(0);
+  const [salesTax, setSalesTax] = useState(0);
+  const [finalTotal, setFinalTotal] = useState(0);
+
+  const calcOrder = (items) => {
+    if (!items.length) return;
+
+    let subTotalAmount = 0;
+
+    items.forEach((item) => {
+      subTotalAmount += item.price * item.quantity;
+    });
+
+    setSubTotal(subTotalAmount);
+    setSalesTax(subTotalAmount * 0.0725);
+    setFinalTotal(subTotalAmount * 1.0725);
+  };
+
+  console.log(
+    `subTotal: ${subTotal}. salesTax: ${salesTax}. finalTotal: ${finalTotal}`
+  );
+
+  return (
+    <CheckoutContext.Provider
+      value={{ subTotal, salesTax, finalTotal, calcOrder }}
+    >
+      {children}
+    </CheckoutContext.Provider>
+  );
+};
+
+CheckoutProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default CheckoutProvider;
