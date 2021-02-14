6a94ec1b0f39ec100ef8068c43c27efd71584464
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-13 17:42:57 -0800

    Implemented function cart contex with button

--- src/Components/ProductDetailsPage.js
+++ src/Components/ProductDetailsPage.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
+import { useCartContext } from '../context/CartContext';
 import { useProductContext } from '../context/ProductContext';
 
 const ProductDetailsPage = () => {
   const { id } = useParams();
+  const { addProduct } = useCartContext();
   const { getProduct, product } = useProductContext();
 
   useEffect(() => {
@@ -30,6 +32,16 @@ const ProductDetailsPage = () => {
             <div className="product-details__category">{product.category}</div>
 
             <div className="product-details__image">{product.image}</div>
+
+            <div className="product-details__cart-add">
+              <button
+                type="button"
+                className="btn"
+                onClick={() => addProduct(product)}
+              >
+                Add to Cart
+              </button>
+            </div>
           </div>
         </>
       )}
