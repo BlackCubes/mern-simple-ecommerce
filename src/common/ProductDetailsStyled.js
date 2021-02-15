ee395d94a72a8270217b91eebb7bae3608d8f250
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-15 07:43:33 -0800

    Created a product details styled component

---
+++ src/common/ProductDetailsStyled.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+export const ProductDetails = styled.div`
+  width: 75%;
+`;
+
+export const ProductDetailsInfo = styled.div`
+  width: 65%;
+  padding-left: 2rem;
+  padding-right: 2rem;
+`;
+
+export const ProductDetailsInfoHeader = styled.div`
+  margin-bottom: 2rem;
+`;
+
+export const ProductDetailsInfoHeaderTitle = styled.div`
+  margin-bottom: 1rem;
+`;
+
+export const ProductDetailsInfoHeaderData = styled.div`
+  padding-top: 1rem;
+`;
+
+export const ProductDetailsInfoImage = styled.div`
+  width: 40rem;
+`;
+
+export const ProductDetailsPriceShippingCart = styled.div`
+  width: 35%;
+  padding-left: 2rem;
+  padding-right: 2rem;
+`;
+
+export const ProductDetailsPriceShippingCartRow = styled.div`
+  margin-top: 2.4rem;
+  padding-top: 2.4rem;
+`;
+
+export const ProductDetailsPrice = styled.div`
+  & h4 {
+    font-weight: 600;
+  }
+`;
+
+export const ProductDetailsShippingBuy = styled.div`
+  margin-bottom: 1rem;
+`;
+
+export const ProductDetailsShippingOption = styled.div`
+  margin-top: 1rem;
+`;
