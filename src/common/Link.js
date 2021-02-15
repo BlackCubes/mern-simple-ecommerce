972e1249c0d2e7c6ecbd3a2361f4410062cfb8a4
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-14 20:43:11 -0800

    Created a link styled component with router-dom link

---
+++ src/common/Link.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link as RouterLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const LinkStyled = styled(RouterLink)`
+  color: ${(props) =>
+    props.colorType ? props.theme.colors[props.colorType] : '#5067c1'};
+  text-decoration: none;
+  background-color: transparent;
+
+  &:hover {
+    color: ${(props) =>
+      props.hoverColorType
+        ? props.theme.colors[props.hoverColorType]
+        : '#425bbb'};
+    text-decoration: underline;
+    outline: 0;
+  }
+`;
+
+const Link = ({ children, rest }) => (
+  <LinkStyled {...rest}>{children}</LinkStyled>
+);
+
+Link.propTypes = {
+  children: PropTypes.node.isRequired,
+  rest: PropTypes.shape({
+    to: PropTypes.string.isRequired,
+    colorType: PropTypes.string,
+    hoverColorType: PropTypes.string,
+  }),
+};
+
+Link.defaultProps = {
+  rest: { to: '#', colorType: null, hoverColorType: null },
+};
+
+export default Link;
