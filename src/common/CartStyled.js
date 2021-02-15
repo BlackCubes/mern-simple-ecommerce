7491119f1e76f3162d9718192d8e94ade22690d3
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-15 15:19:06 -0800

    Added more stylings

--- src/common/CartStyled.js
+++ src/common/CartStyled.js
@@ -56,11 +56,18 @@ export const CartCardShippingInfo = styled.div`
 `;
 
 export const CartCardAction = styled.div`
-  width: 25%;
+  width: 20%;
 `;
 
 export const CartCardQuantity = styled.div`
   margin-bottom: 1rem;
+  padding-left: 0.5rem;
+  padding-right: 0.5rem;
+
+  & p {
+    display: flex;
+    justify-content: space-between;
+  }
 
   & span {
     font-weight: 600;
@@ -68,6 +75,8 @@ export const CartCardQuantity = styled.div`
 `;
 
 export const CartCardRaiseLower = styled.div`
+  display: flex;
+  justify-content: space-between;
   margin-bottom: 1rem;
 `;
 
@@ -76,5 +85,6 @@ export const CartCardRemove = styled.div`
 `;
 
 export const CartCardPrice = styled.div`
-  width: 13%;
+  width: 17%;
+  text-align: right;
 `;
