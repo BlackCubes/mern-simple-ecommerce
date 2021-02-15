f0b5dba39f2173307ec74bfe814577b212b46665
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-15 13:49:53 -0800

    Added another prop with styled conditional

--- src/common/Paragraph.js
+++ src/common/Paragraph.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const ParagraphStyled = styled.p`
   font-size: 1.6rem;
+
+  ${({ tagtype }) =>
+    tagtype &&
+    css`
+      font-weight: 600;
+    `}
 `;
 
-const Paragraph = ({ children }) => (
-  <ParagraphStyled>{children}</ParagraphStyled>
+const Paragraph = ({ children, tagtype }) => (
+  <ParagraphStyled tagtype={tagtype}>{children}</ParagraphStyled>
 );
 
 Paragraph.propTypes = {
   children: PropTypes.node.isRequired,
+  tagtype: PropTypes.string,
 };
+
+Paragraph.defaultProps = { tagtype: null };
 
 export default Paragraph;
