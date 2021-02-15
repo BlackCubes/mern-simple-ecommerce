e2b30a940db5f2021c7768ae2ac5753ee819e495
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-14 21:23:58 -0800

    Added two new colors

--- src/common/theme.js
+++ src/common/theme.js
@@ -4,6 +4,8 @@ export default {
     vivid_pink: '#ff1e5e',
     vivid_pink_tone: '#f62762',
     moderate_blue: '#505cc1',
+    moderate_blue_2: '#5067c1',
+    moderate_blue_dark: '#334690',
     lime_green: '#37bd6c',
   },
 };
