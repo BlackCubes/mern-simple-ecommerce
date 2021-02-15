92094aecb11a8d12c5041e1fd246287f4c0c758d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-14 17:54:23 -0800

    Created two utility functions for datetime

--- src/utils/index.js
+++ src/utils/index.js
@@ -21,4 +21,13 @@ const getProductAPI = async (id) => {
   }
 };
 
+// DATETIME
+export const daysFromNow = (days) => {
+  const currentDay = new Date();
+  return currentDay.setDate(currentDay.getDate() + days);
+};
+
+export const dateTimeFormat = (language, options, format) =>
+  new Intl.DateTimeFormat(language, options).format(format);
+
 export { getProductsAPI, getProductAPI };
