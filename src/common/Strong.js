c8560dbebbc150e3712bb63e9558734f4d4336e9
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-14 16:46:31 -0800

    Created a small styled component

---
+++ src/common/Strong.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
+
+const SmallStyled = styled.small`
+  font-size: 1.3rem;
+
+  ${({ tagType }) =>
+    tagType &&
+    css`
+      font-weight: 600;
+    `}
+`;
+
+const Small = ({ children, tagType }) => (
+  <SmallStyled tagType={tagType}>{children}</SmallStyled>
+);
+
+Small.propTypes = {
+  children: PropTypes.node.isRequired,
+  tagType: PropTypes.string,
+};
+
+Small.defaultProps = {
+  tagType: null,
+};
+
+export default Small;
