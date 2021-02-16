23433eaa4cc2e03f2c63622e09077fdd79c7f590
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-16 00:09:07 -0800

    Implemented category sidebar component, and refactored

--- src/Components/ProductDetailsPage.js
+++ src/Components/ProductDetailsPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { Link as RouterLink, useParams } from 'react-router-dom';
 
+import CategorySidebar from './CategorySidebar';
+
 import {
   Button,
   HeadingQuaternary,
@@ -26,86 +28,87 @@ const ProductDetailsPage = () => {
   }, [id]);
 
   return (
-    <>
-      {!product ? (
-        <HeadingQuaternary>Loading...</HeadingQuaternary>
-      ) : (
-        <>
-          <ProductDetailsStyled.ProductDetailsContainer>
-            <RouterLink to="/products">Back</RouterLink>
+    <ProductDetailsStyled.ProductDetailsContainer>
+      <RouterLink to="/products">Back</RouterLink>
 
-            <ProductDetailsStyled.ProductDetails>
-              <ProductDetailsStyled.ProductDetailsHeader>
-                <ProductDetailsStyled.ProductDetailsHeaderTitle>
-                  <HeadingQuaternary>{product.title}</HeadingQuaternary>
-                </ProductDetailsStyled.ProductDetailsHeaderTitle>
+      <ProductDetailsStyled.ProductDetails>
+        {!product ? (
+          <HeadingQuaternary>Loading...</HeadingQuaternary>
+        ) : (
+          <>
+            <ProductDetailsStyled.ProductDetailsHeader>
+              <ProductDetailsStyled.ProductDetailsHeaderTitle>
+                <HeadingQuaternary>{product.title}</HeadingQuaternary>
+              </ProductDetailsStyled.ProductDetailsHeaderTitle>
 
-                <ProductDetailsStyled.ProductDetailsHeaderData>
-                  <Small tagType="strong">Type:</Small>
-                  &nbsp;
-                  <Small>{product.category}</Small>
-                </ProductDetailsStyled.ProductDetailsHeaderData>
-              </ProductDetailsStyled.ProductDetailsHeader>
+              <ProductDetailsStyled.ProductDetailsHeaderData>
+                <Small tagType="strong">Type:</Small>
+                &nbsp;
+                <Small>{product.category}</Small>
+              </ProductDetailsStyled.ProductDetailsHeaderData>
+            </ProductDetailsStyled.ProductDetailsHeader>
 
-              <ProductDetailsStyled.ProductDetailsBottomContainer>
-                <ProductDetailsStyled.ProductDetailsInfo>
-                  <ProductDetailsStyled.ProductDetailsInfoImage>
-                    <Image rest={{ src: product.image, alt: product.title }} />
-                  </ProductDetailsStyled.ProductDetailsInfoImage>
-                </ProductDetailsStyled.ProductDetailsInfo>
+            <ProductDetailsStyled.ProductDetailsBottomContainer>
+              <ProductDetailsStyled.ProductDetailsInfo>
+                <ProductDetailsStyled.ProductDetailsInfoImage>
+                  <Image rest={{ src: product.image, alt: product.title }} />
+                </ProductDetailsStyled.ProductDetailsInfoImage>
+              </ProductDetailsStyled.ProductDetailsInfo>
 
-                <ProductDetailsStyled.ProductDetailsPriceShippingCart>
-                  <ProductDetailsStyled.ProductDetailsPrice>
-                    <HeadingSecondary>{`$${product.price}`}</HeadingSecondary>
-                  </ProductDetailsStyled.ProductDetailsPrice>
+              <ProductDetailsStyled.ProductDetailsPriceShippingCart>
+                <ProductDetailsStyled.ProductDetailsPrice>
+                  <HeadingSecondary>{`$${product.price}`}</HeadingSecondary>
+                </ProductDetailsStyled.ProductDetailsPrice>
 
-                  <ProductDetailsStyled.ProductDetailsPriceShippingCartRow>
-                    <ProductDetailsStyled.ProductDetailsShippingBuy>
-                      <Small tagType="strong" colorType="lime_green">
-                        Get it in 3 days
-                      </Small>
-                    </ProductDetailsStyled.ProductDetailsShippingBuy>
+                <ProductDetailsStyled.ProductDetailsPriceShippingCartRow>
+                  <ProductDetailsStyled.ProductDetailsShippingBuy>
+                    <Small tagType="strong" colorType="lime_green">
+                      Get it in 3 days
+                    </Small>
+                  </ProductDetailsStyled.ProductDetailsShippingBuy>
 
-                    <ProductDetailsStyled.ProductDetailsShippingOption>
-                      <Small tagType="strong">
-                        {product.price >= 50
-                          ? 'FREE Shipping:'
-                          : '3-DAY Shipping:'}
-                      </Small>
-                      &nbsp;
-                      <Small>
-                        Get it by&nbsp;
-                        {dateTimeFormat(
-                          'en-US',
-                          { weekday: 'short' },
-                          daysFromNow(3)
-                        )}
-                      </Small>
-                    </ProductDetailsStyled.ProductDetailsShippingOption>
-                  </ProductDetailsStyled.ProductDetailsPriceShippingCartRow>
+                  <ProductDetailsStyled.ProductDetailsShippingOption>
+                    <Small tagType="strong">
+                      {product.price >= 50
+                        ? 'FREE Shipping:'
+                        : '3-DAY Shipping:'}
+                    </Small>
+                    &nbsp;
+                    <Small>
+                      Get it by&nbsp;
+                      {dateTimeFormat(
+                        'en-US',
+                        { weekday: 'short' },
+                        daysFromNow(3)
+                      )}
+                    </Small>
+                  </ProductDetailsStyled.ProductDetailsShippingOption>
+                </ProductDetailsStyled.ProductDetailsPriceShippingCartRow>
 
-                  <ProductDetailsStyled.ProductDetailsPriceShippingCartRow>
-                    <div className="product-details__cart-add">
-                      <Button
-                        rest={{
-                          type: 'button',
-                          onClick: () => addProduct(product),
-                        }}
-                      >
-                        Add to Cart
-                      </Button>
-                    </div>
-                  </ProductDetailsStyled.ProductDetailsPriceShippingCartRow>
-                </ProductDetailsStyled.ProductDetailsPriceShippingCart>
-              </ProductDetailsStyled.ProductDetailsBottomContainer>
-            </ProductDetailsStyled.ProductDetails>
-          </ProductDetailsStyled.ProductDetailsContainer>
-          <div className="product-details__description">
-            {product.description}
-          </div>
-        </>
-      )}
-    </>
+                <ProductDetailsStyled.ProductDetailsPriceShippingCartRow>
+                  <div className="product-details__cart-add">
+                    <Button
+                      rest={{
+                        type: 'button',
+                        onClick: () => addProduct(product),
+                      }}
+                    >
+                      Add to Cart
+                    </Button>
+                  </div>
+                </ProductDetailsStyled.ProductDetailsPriceShippingCartRow>
+              </ProductDetailsStyled.ProductDetailsPriceShippingCart>
+            </ProductDetailsStyled.ProductDetailsBottomContainer>
+
+            <div className="product-details__description">
+              {product.description}
+            </div>
+          </>
+        )}
+      </ProductDetailsStyled.ProductDetails>
+
+      <CategorySidebar />
+    </ProductDetailsStyled.ProductDetailsContainer>
   );
 };
 
