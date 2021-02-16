06ee949b5c0b40da00931e1c951b7b85cf49b88f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-15 17:39:06 -0800

    Added two styled components

---
+++ src/common/MainSideNav.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const MainSideNav = styled.div`
+  width: 25%;
+  padding-left: 4rem;
+`;
+
+export const MainSideNavContent = styled.div`
+  height: 100%;
+`;
