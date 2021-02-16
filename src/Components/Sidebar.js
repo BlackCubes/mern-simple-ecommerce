14d933f3c649076bfb593cd3b90e66c8ba290ac1
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-15 21:47:36 -0800

    Added custom styling component

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -11,22 +11,24 @@ const Sidebar = () => {
   return (
     <SidebarStyled.Sidebar>
       <SidebarStyled.SidebarContainer>
-        <SidebarStyled.SidebarFacet>
-          <SidebarStyled.SidebarFacetLegend>
-            <Paragraph tagtype="strong">Category</Paragraph>
-          </SidebarStyled.SidebarFacetLegend>
+        <SidebarStyled.SidebarContent>
+          <SidebarStyled.SidebarFacet>
+            <SidebarStyled.SidebarFacetLegend>
+              <Paragraph tagtype="strong">Category</Paragraph>
+            </SidebarStyled.SidebarFacetLegend>
 
-          <SidebarStyled.SidebarFacetList>
-            {categories &&
-              categories.map((prop) => (
-                <SidebarStyled.SidebarFacetListItem key={uuidv4()}>
-                  <Small>
-                    <Link href={`/products/category/${prop}`}>{prop}</Link>
-                  </Small>
-                </SidebarStyled.SidebarFacetListItem>
-              ))}
-          </SidebarStyled.SidebarFacetList>
-        </SidebarStyled.SidebarFacet>
+            <SidebarStyled.SidebarFacetList>
+              {categories &&
+                categories.map((prop) => (
+                  <SidebarStyled.SidebarFacetListItem key={uuidv4()}>
+                    <Small>
+                      <Link href={`/products/category/${prop}`}>{prop}</Link>
+                    </Small>
+                  </SidebarStyled.SidebarFacetListItem>
+                ))}
+            </SidebarStyled.SidebarFacetList>
+          </SidebarStyled.SidebarFacet>
+        </SidebarStyled.SidebarContent>
       </SidebarStyled.SidebarContainer>
     </SidebarStyled.Sidebar>
   );
