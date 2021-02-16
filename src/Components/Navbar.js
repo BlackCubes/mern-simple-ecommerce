5774993735e6fd259609e88ed6e180be1d06a691
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-16 12:59:45 -0800

    Recreated the navbar with custom styled components

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
+
+import { Image, NavbarStyled } from '../common';
 
 const Navbar = () => (
-  <nav>
-    <Link to="/products">Products</Link>
-    <Link to="/cart">Cart</Link>
-  </nav>
+  <NavbarStyled.Navbar>
+    <NavbarStyled.NavbarContainer>
+      <NavbarStyled.NavbarLogo>
+        <RouterLink to="/">
+          <Image rest={{ src: '/img/icon.png', alt: 'Company Logo' }} />
+        </RouterLink>
+      </NavbarStyled.NavbarLogo>
+
+      <NavbarStyled.NavbarMenuHolder>
+        <NavbarStyled.NavbarMenu>
+          <NavbarStyled.NavbarMenuList>
+            <NavbarStyled.NavbarMenuListItem>
+              Hello!
+            </NavbarStyled.NavbarMenuListItem>
+          </NavbarStyled.NavbarMenuList>
+        </NavbarStyled.NavbarMenu>
+      </NavbarStyled.NavbarMenuHolder>
+    </NavbarStyled.NavbarContainer>
+  </NavbarStyled.Navbar>
+  // <nav>
+  //   <Link to="/products">Products</Link>
+  //   <Link to="/cart">Cart</Link>
+  // </nav>
 );
 
 export default Navbar;
