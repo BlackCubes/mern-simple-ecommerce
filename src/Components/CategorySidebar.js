5e2661f407f83c4b040eeb5444884389482b31fb
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-15 23:58:21 -0800

    Created a category sidebar component using context and custome styled components

---
+++ src/Components/CategorySidebar.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+
+import {
+  CategorySidebarStyled,
+  Image,
+  Link,
+  Paragraph,
+  SidebarStyled,
+} from '../common';
+
+import { useProductContext } from '../context/ProductContext';
+
+const CategorySidebar = () => {
+  const [similarProducts, setSimilarProducts] = useState(null);
+  const { products, category } = useProductContext();
+
+  const categoryProducts = (productCategory) => {
+    const similar = products.filter(
+      (product) => product.category === productCategory
+    );
+    setSimilarProducts(similar);
+  };
+
+  useEffect(() => {
+    if (category) categoryProducts(category);
+  }, [category]);
+
+  return (
+    <SidebarStyled.Sidebar>
+      <SidebarStyled.SidebarContainer>
+        <CategorySidebarStyled.CategorySidebar>
+          <CategorySidebarStyled.CategorySidebarHeader>
+            <Paragraph tagtype="strong">Similar Category</Paragraph>
+          </CategorySidebarStyled.CategorySidebarHeader>
+
+          {similarProducts &&
+            similarProducts.map((prop) => (
+              <CategorySidebarStyled.CategorySidebarCard key={prop.id}>
+                <CategorySidebarStyled.CategorySidebarCardImage>
+                  <RouterLink to={`/products/${prop.id}`}>
+                    <Image rest={{ src: prop.image, alt: prop.title }} />
+                  </RouterLink>
+                </CategorySidebarStyled.CategorySidebarCardImage>
+
+                <CategorySidebarStyled.CategorySidebarCardInfo>
+                  <CategorySidebarStyled.CategorySidebarCardInfoTitle>
+                    <Paragraph>
+                      <Link href={`/products/${prop.id}`}>{prop.title}</Link>
+                    </Paragraph>
+                  </CategorySidebarStyled.CategorySidebarCardInfoTitle>
+
+                  <CategorySidebarStyled.CategorySidebarCardInfoPrice>
+                    <Paragraph tagtype="strong">{prop.price}</Paragraph>
+                  </CategorySidebarStyled.CategorySidebarCardInfoPrice>
+                </CategorySidebarStyled.CategorySidebarCardInfo>
+              </CategorySidebarStyled.CategorySidebarCard>
+            ))}
+        </CategorySidebarStyled.CategorySidebar>
+      </SidebarStyled.SidebarContainer>
+    </SidebarStyled.Sidebar>
+  );
+};
+
+export default CategorySidebar;
