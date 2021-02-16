a4460cf2d151c9806a185a7730d9a891a89d29a4
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-15 20:48:43 -0800

    Created a sidebar component with context and custome styled components

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import { Link, Paragraph, SidebarStyled, Small } from '../common';
+
+import { useProductContext } from '../context/ProductContext';
+
+const Sidebar = () => {
+  const { categories, getCategory } = useProductContext();
+
+  const handleClick = (selectedCategory) => getCategory(selectedCategory);
+
+  return (
+    <SidebarStyled.Sidebar>
+      <SidebarStyled.SidebarContainer>
+        <SidebarStyled.SidebarFacet>
+          <SidebarStyled.SidebarFacetLegend>
+            <Paragraph tagtype="strong">Category</Paragraph>
+          </SidebarStyled.SidebarFacetLegend>
+
+          <SidebarStyled.SidebarFacetList>
+            {categories &&
+              categories.map((prop) => (
+                <SidebarStyled.SidebarFacetListItem key={uuidv4()}>
+                  <Small>
+                    <Link href={() => handleClick(prop)}>{prop}</Link>
+                  </Small>
+                </SidebarStyled.SidebarFacetListItem>
+              ))}
+          </SidebarStyled.SidebarFacetList>
+        </SidebarStyled.SidebarFacet>
+      </SidebarStyled.SidebarContainer>
+    </SidebarStyled.Sidebar>
+  );
+};
+
+export default Sidebar;
