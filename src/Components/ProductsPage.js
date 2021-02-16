2ddc68d493e9932d1bde0d73627efa37cc767f70
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-15 20:55:13 -0800

    Implemented sidebar and custome styled component

--- src/Components/ProductsPage.js
+++ src/Components/ProductsPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 
+import Sidebar from './Sidebar';
+
 import {
   Button,
   HeadingQuaternary,
@@ -22,71 +24,77 @@ const ProductsPage = () => {
   const { products } = useProductContext();
 
   return (
-    <ProductsStyled.Products>
-      {!products ? (
-        <HeadingSecondary>Hello!</HeadingSecondary>
-      ) : (
-        products.map((prop, key) => (
-          <ProductsStyled.ProductsCard key={prop.id}>
-            <ProductsStyled.ProductsCardImage>
-              <RouterLink to={`/products/${prop.id}`}>
-                <Image rest={{ src: prop.image, alt: prop.title }} />
-              </RouterLink>
-            </ProductsStyled.ProductsCardImage>
+    <ProductsStyled.ProductsContainer>
+      <Sidebar />
 
-            <ProductsStyled.ProductsCardRightColumn>
-              <ProductsStyled.ProductsCardInfo>
-                <ProductsStyled.ProductsCardInfoTitle>
-                  <HeadingQuaternary>
-                    <Link href={`/products/${prop.id}`}>{prop.title}</Link>
-                  </HeadingQuaternary>
-                </ProductsStyled.ProductsCardInfoTitle>
+      <ProductsStyled.Products>
+        {!products ? (
+          <HeadingSecondary>Hello!</HeadingSecondary>
+        ) : (
+          products.map((prop, key) => (
+            <ProductsStyled.ProductsCard key={prop.id}>
+              <ProductsStyled.ProductsCardImage>
+                <RouterLink to={`/products/${prop.id}`}>
+                  <Image rest={{ src: prop.image, alt: prop.title }} />
+                </RouterLink>
+              </ProductsStyled.ProductsCardImage>
 
-                <ProductsStyled.ProductsCardInfoShipping>
-                  <ProductsStyled.ProductsCardInfoShippingBuy>
-                    <Small tagType="strong" colorType="lime_green">
-                      Buy it today
-                    </Small>
-                  </ProductsStyled.ProductsCardInfoShippingBuy>
+              <ProductsStyled.ProductsCardRightColumn>
+                <ProductsStyled.ProductsCardInfo>
+                  <ProductsStyled.ProductsCardInfoTitle>
+                    <HeadingQuaternary>
+                      <Link href={`/products/${prop.id}`}>{prop.title}</Link>
+                    </HeadingQuaternary>
+                  </ProductsStyled.ProductsCardInfoTitle>
 
-                  <ProductsStyled.ProductsCardInfoShippingOption>
-                    <Small tagType="strong">
-                      {prop.price >= 50 ? 'FREE Shipping:' : '3-DAY Shipping:'}
-                    </Small>
-                    &nbsp;
-                    <Small>
-                      Get it by&nbsp;
-                      {dateTimeFormat(
-                        'en-US',
-                        { weekday: 'short' },
-                        daysFromNow(3)
-                      )}
-                    </Small>
-                  </ProductsStyled.ProductsCardInfoShippingOption>
-                </ProductsStyled.ProductsCardInfoShipping>
-              </ProductsStyled.ProductsCardInfo>
+                  <ProductsStyled.ProductsCardInfoShipping>
+                    <ProductsStyled.ProductsCardInfoShippingBuy>
+                      <Small tagType="strong" colorType="lime_green">
+                        Buy it today
+                      </Small>
+                    </ProductsStyled.ProductsCardInfoShippingBuy>
 
-              <ProductsStyled.ProductsCardPriceCart>
-                <ProductsStyled.ProductsCardPrice>
-                  <HeadingQuaternary>{`$${prop.price}`}</HeadingQuaternary>
-                </ProductsStyled.ProductsCardPrice>
+                    <ProductsStyled.ProductsCardInfoShippingOption>
+                      <Small tagType="strong">
+                        {prop.price >= 50
+                          ? 'FREE Shipping:'
+                          : '3-DAY Shipping:'}
+                      </Small>
+                      &nbsp;
+                      <Small>
+                        Get it by&nbsp;
+                        {dateTimeFormat(
+                          'en-US',
+                          { weekday: 'short' },
+                          daysFromNow(3)
+                        )}
+                      </Small>
+                    </ProductsStyled.ProductsCardInfoShippingOption>
+                  </ProductsStyled.ProductsCardInfoShipping>
+                </ProductsStyled.ProductsCardInfo>
 
-                <ProductsStyled.ProductsCardCartBtn>
-                  <Button
-                    rest={{
-                      type: 'button',
-                      onClick: () => addProduct(products[key]),
-                    }}
-                  >
-                    Add to Cart
-                  </Button>
-                </ProductsStyled.ProductsCardCartBtn>
-              </ProductsStyled.ProductsCardPriceCart>
-            </ProductsStyled.ProductsCardRightColumn>
-          </ProductsStyled.ProductsCard>
-        ))
-      )}
-    </ProductsStyled.Products>
+                <ProductsStyled.ProductsCardPriceCart>
+                  <ProductsStyled.ProductsCardPrice>
+                    <HeadingQuaternary>{`$${prop.price}`}</HeadingQuaternary>
+                  </ProductsStyled.ProductsCardPrice>
+
+                  <ProductsStyled.ProductsCardCartBtn>
+                    <Button
+                      rest={{
+                        type: 'button',
+                        onClick: () => addProduct(products[key]),
+                      }}
+                    >
+                      Add to Cart
+                    </Button>
+                  </ProductsStyled.ProductsCardCartBtn>
+                </ProductsStyled.ProductsCardPriceCart>
+              </ProductsStyled.ProductsCardRightColumn>
+            </ProductsStyled.ProductsCard>
+          ))
+        )}
+      </ProductsStyled.Products>
+    </ProductsStyled.ProductsContainer>
   );
 };
 
