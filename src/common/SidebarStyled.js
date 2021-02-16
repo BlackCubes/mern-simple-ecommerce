0925cb1b9d4ab376c07edc8b0c34cb39ae984481
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-15 20:06:34 -0800

    Created more custom styled components

--- src/common/SidebarStyled.js
+++ src/common/SidebarStyled.js
@@ -8,3 +8,28 @@ export const Sidebar = styled.div`
 export const SidebarContainer = styled.div`
   height: 100%;
 `;
+
+export const SidebarContent = styled.div`
+  position: sticky;
+  top: 1.5rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
+`;
+
+export const SidebarFacet = styled.div`
+  padding: 2rem 0 2rem 2rem;
+  border-bottom: 1px solid #000;
+`;
+
+export const SidebarFacetLegend = styled.div`
+  padding-bottom: 1rem;
+`;
+
+export const SidebarFacetList = styled.ul`
+  margin-top: 1rem;
+  list-style-type: none;
+`;
+
+export const SidebarFacetListItem = styled.li`
+  margin-bottom: 1rem;
+`;
