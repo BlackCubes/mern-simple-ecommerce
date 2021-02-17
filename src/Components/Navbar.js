4a5549b00c2b7384e5024ba173ac844c14da4b1c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-16 22:54:30 -0800

    Implemented cart context and dynamically displayed the cart amount

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,63 +3,69 @@ import { Link as RouterLink } from 'react-router-dom';
 
 import { Image, Link, NavbarStyled } from '../common';
 
-const Navbar = () => (
-  <NavbarStyled.Navbar>
-    <NavbarStyled.NavbarContainer>
-      <NavbarStyled.NavbarLogo>
-        <RouterLink to="/">
-          <Image rest={{ src: '/img/icon.png', alt: 'Company Logo' }} />
-        </RouterLink>
-      </NavbarStyled.NavbarLogo>
+import { useCartContext } from '../context/CartContext';
 
-      <NavbarStyled.NavbarMenuHolder>
-        <NavbarStyled.NavbarMenu>
-          <NavbarStyled.NavbarMenuList>
-            <NavbarStyled.NavbarMenuListItem>
-              <Link
-                href="/products"
-                rest={{
-                  colortype: 'white',
-                  hovercolortype: 'vivid_pink',
-                }}
-              >
-                Products
-              </Link>
-            </NavbarStyled.NavbarMenuListItem>
+const Navbar = () => {
+  const { cart } = useCartContext();
 
-            <NavbarStyled.NavbarMenuListItem>
-              <Link
-                href="/cart"
-                rest={{
-                  colortype: 'white',
-                  hovercolortype: 'vivid_pink',
-                }}
-              >
-                <NavbarStyled.NavbarCart>
-                  <NavbarStyled.NavbarCartImage>
-                    <Image
-                      rest={{
-                        src: '/img/shopping-cart.svg',
-                        alt: 'Shopping Cart',
-                      }}
-                    />
-                  </NavbarStyled.NavbarCartImage>
+  return (
+    <NavbarStyled.Navbar>
+      <NavbarStyled.NavbarContainer>
+        <NavbarStyled.NavbarLogo>
+          <RouterLink to="/">
+            <Image rest={{ src: '/img/icon.png', alt: 'Company Logo' }} />
+          </RouterLink>
+        </NavbarStyled.NavbarLogo>
 
-                  <NavbarStyled.NavbarCartLabel>
-                    Cart
-                  </NavbarStyled.NavbarCartLabel>
+        <NavbarStyled.NavbarMenuHolder>
+          <NavbarStyled.NavbarMenu>
+            <NavbarStyled.NavbarMenuList>
+              <NavbarStyled.NavbarMenuListItem>
+                <Link
+                  href="/products"
+                  rest={{
+                    colortype: 'white',
+                    hovercolortype: 'vivid_pink',
+                  }}
+                >
+                  Products
+                </Link>
+              </NavbarStyled.NavbarMenuListItem>
 
-                  <NavbarStyled.NavbarCartQuantity>
-                    2
-                  </NavbarStyled.NavbarCartQuantity>
-                </NavbarStyled.NavbarCart>
-              </Link>
-            </NavbarStyled.NavbarMenuListItem>
-          </NavbarStyled.NavbarMenuList>
-        </NavbarStyled.NavbarMenu>
-      </NavbarStyled.NavbarMenuHolder>
-    </NavbarStyled.NavbarContainer>
-  </NavbarStyled.Navbar>
-);
+              <NavbarStyled.NavbarMenuListItem>
+                <Link
+                  href="/cart"
+                  rest={{
+                    colortype: 'white',
+                    hovercolortype: 'vivid_pink',
+                  }}
+                >
+                  <NavbarStyled.NavbarCart>
+                    <NavbarStyled.NavbarCartImage>
+                      <Image
+                        rest={{
+                          src: '/img/shopping-cart.svg',
+                          alt: 'Shopping Cart',
+                        }}
+                      />
+                    </NavbarStyled.NavbarCartImage>
+
+                    <NavbarStyled.NavbarCartLabel>
+                      Cart
+                    </NavbarStyled.NavbarCartLabel>
+
+                    <NavbarStyled.NavbarCartQuantity>
+                      {cart.length}
+                    </NavbarStyled.NavbarCartQuantity>
+                  </NavbarStyled.NavbarCart>
+                </Link>
+              </NavbarStyled.NavbarMenuListItem>
+            </NavbarStyled.NavbarMenuList>
+          </NavbarStyled.NavbarMenu>
+        </NavbarStyled.NavbarMenuHolder>
+      </NavbarStyled.NavbarContainer>
+    </NavbarStyled.Navbar>
+  );
+};
 
 export default Navbar;
