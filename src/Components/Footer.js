bb34df52d4b730be947e33e5af0221269ba13443
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-17 07:18:54 -0800

    Added more content

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { FooterStyled, Link } from '../common';
+import { FooterStyled, Link, Small } from '../common';
 
 library.add(faGithub, faLinkedin);
 
@@ -30,6 +30,23 @@ const Footer = () => (
         </FooterStyled.FooterFollowList>
       </FooterStyled.FooterFollowNavigation>
     </FooterStyled.FooterFollow>
+
+    <FooterStyled.FooterLower>
+      <FooterStyled.FooterCopyright>
+        <FooterStyled.FooterCopyrightContent>
+          <Small sizetype="xxsmall">
+            Email:&nbsp;
+            <Link href="mailto:gutierrezelias1991@gmail.com">
+              gutierrezelias1991@gmail.com
+            </Link>
+            &nbsp;&nbsp; | &nbsp; Phone:&nbsp;
+            <Link href="tel:5595953839">559-595-3839</Link>
+            <br />
+            3pm - 8pm PST Mon-Fri &nbsp; 10am - 4pm Sat&Sun
+          </Small>
+        </FooterStyled.FooterCopyrightContent>
+      </FooterStyled.FooterCopyright>
+    </FooterStyled.FooterLower>
   </FooterStyled.Footer>
 );
 
