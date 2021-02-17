601ec0942caca68bc62f68def711dbd6032bdcc1
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-16 16:12:02 -0800

    Changed some stylings and made new ones

--- src/common/NavbarStyled.js
+++ src/common/NavbarStyled.js
@@ -2,23 +2,29 @@ import styled from 'styled-components';
 
 export const Navbar = styled.header`
   width: 100%;
-  height: 8rem;
+  height: 11rem;
 `;
 
 export const NavbarContainer = styled.div`
+  position: relative;
   width: 98%;
   max-width: 145rem;
+  min-height: 11rem;
   margin-left: auto;
   margin-right: auto;
 `;
 
 export const NavbarLogo = styled.div`
   float: left;
-  width: 3rem;
+  width: 7rem;
   position: absolute;
   line-height: 0;
   transform: translateY(-50%);
   top: 50%;
+
+  & a {
+    display: block;
+  }
 `;
 
 export const NavbarMenuHolder = styled.div`
