b9aff600d5c72a477e3a57e1afb2875f19d500d9
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-17 10:59:50 -0800

    Added red color

--- src/common/theme.js
+++ src/common/theme.js
@@ -3,6 +3,7 @@ export default {
     black: '#000',
     black_gray: '#111111',
     white: '#fff',
+    red: '#ff0000',
     vivid_pink: '#ff1e5e',
     vivid_pink_tone: '#f62762',
     moderate_blue: '#505cc1',
