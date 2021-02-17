1605f5022ebc46ba22c90912a95559fa51d883d1
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-16 17:59:58 -0800

    Added new colors

--- src/common/theme.js
+++ src/common/theme.js
@@ -1,6 +1,8 @@
 export default {
   colors: {
     black: '#000',
+    black-gray: '#111111',
+    white: '#fff',
     vivid_pink: '#ff1e5e',
     vivid_pink_tone: '#f62762',
     moderate_blue: '#505cc1',
