63a058ab05a59d24e22047629015f69c9ad19252
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-17 00:10:15 -0800

    Added some of the custom styled components

---
+++ src/common/FooterStyled.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+export const Footer = styled.div`
+  font-family: inherit;
+  color: ${(props) => props.theme.colors.white};
+  background-color: ${(props) => props.theme.colors.black_gray};
+`;
+
+export const FooterFollow = styled.div`
+  padding: 2rem;
+  background-color: ${(props) => props.theme.colors.lime_green};
+  list-style: none;
+  text-align: center;
+`;
+
+export const FooterFollowHeader = styled.div`
+  font-size: 1.6rem;
+  padding-bottom: 1rem;
+`;
+
+export const FooterFollowNavigation = styled.div`
+  margin-top: 0.5rem;
+`;
+
+export const FooterFollowList = styled.ul`
+  margin: 0;
+  padding: 0;
+`;
+
+export const FooterFollowItem = styled.li`
+  display: inline-block;
+
+  & a {
+    display: block;
+    margin: 0 1rem;
+    padding: 0.6rem;
+    color: ${(props) => props.theme.colors.white};
+    background-color: ${(props) => props.theme.colors.black_gray};
+    border-radius: 50%;
+  }
+
+  & i {
+    font-size: 3.2rem;
+    width: 3.1rem;
+    height: 3.1rem;
+  }
+`;
