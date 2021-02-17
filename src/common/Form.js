82ba115d63d072458233cf935f24fa8585c5c63a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-17 14:06:41 -0800

    Added handleSubmit function, and changed prop

--- src/common/Form.js
+++ src/common/Form.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 // import styled from 'styled-components';
 import { v4 as uuidv4 } from 'uuid';
 
-const Form = ({ handleSubmit, formFields, InputComponent }) => {
+const Form = ({ onSubmit, formFields, InputComponent }) => {
   const [values, setValues] = useState({});
   // const [errors, setErrors] = useState({});
   const errors = {};
@@ -12,6 +12,11 @@ const Form = ({ handleSubmit, formFields, InputComponent }) => {
     const { name, value } = e.target;
 
     setValues((val) => ({ ...val, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(values);
   };
 
   return (
@@ -30,7 +35,7 @@ const Form = ({ handleSubmit, formFields, InputComponent }) => {
 };
 
 Form.propTypes = {
-  handleSubmit: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
   formFields: PropTypes.arrayOf(PropTypes.object).isRequired,
   InputComponent: PropTypes.elementType.isRequired,
 };
