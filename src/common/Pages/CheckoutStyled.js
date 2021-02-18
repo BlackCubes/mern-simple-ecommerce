b986c1815849002d355894a5507dba246f3c0ce4
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-18 12:39:54 -0800

    Added some of the custom styled components

---
+++ src/common/Pages/CheckoutStyled.js
@@ -0,0 +1,93 @@
+import styled from 'styled-components';
+
+export const CheckoutStyled = styled.div`
+  width: 100rem;
+  margin: 0 auto;
+  padding: 1.5rem;
+`;
+
+export const CheckoutPrimaryStyled = styled.div`
+  float: left;
+  width: 67%;
+`;
+
+export const CheckoutPageHeaderStyled = styled.div`
+  padding-left: 1.5rem;
+
+  & h2 {
+    line-height: 3.3rem;
+  }
+`;
+
+export const CheckoutShippingCardStyled = styled.div`
+  margin: 3rem 1.5rem;
+  padding: 3.2rem 2.4rem;
+`;
+
+export const CheckoutShippingCardHeaderStyled = styled.div`
+  margin-bottom: 1rem;
+
+  & h4 {
+    font-weight: 600;
+  }
+`;
+
+export const CheckoutShippingInfoStyled = styled.div`
+  margin: 1.5rem 0;
+`;
+
+export const CheckoutShippingInfoSavedStyled = styled.div`
+  margin-bottom: 1rem;
+
+  & p {
+    font-size: 1.4rem;
+  }
+`;
+
+export const CheckoutShippingInfoAddStyled = styled.div`
+  margin-top: 0.5rem;
+
+  & span {
+    font-size: 1.3rem !important;
+  }
+`;
+
+export const CheckoutShippingOptionsStyled = styled.div`
+  margin: 1rem 0 2rem;
+`;
+
+export const CheckoutShippingOptionsHeaderStyled = styled.div`
+  margin-bottom: -0.9rem;
+`;
+
+export const CheckoutShippingOptionsListItemStyled = styled.div`
+  position: relative;
+  margin-top: 2rem;
+  padding-right: 0.4rem;
+`;
+
+export const CheckoutShippingOptionsListPriceStyled = styled.div`
+  position: absolute;
+  top: 1rem;
+  right: 0;
+`;
+
+export const CheckoutCardRowStyled = styled.div`
+  margin-left: -1.5rem;
+  margin-right: -1.5rem;
+`;
+
+export const CheckoutCardColStyled = styled.div`
+  float: left;
+  width: 50%;
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+`;
+
+export const CheckoutSecondaryStyled = styled.div`
+  float: left;
+  width: 33%;
+  margin-top: 6.3rem;
+  padding-left: 1.5rem;
+  padding-right: 1.5rem;
+`;
