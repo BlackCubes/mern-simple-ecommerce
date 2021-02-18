c221d7a853fff7015b44e42279f4bc36bb8fb925
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-18 00:19:30 -0800

    Imported and exported module

---
+++ src/Components/index.js
@@ -0,0 +1,7 @@
+import CategorySidebar from './CategorySidebar';
+import CheckoutSidebar from './CheckoutSidebar';
+import Footer from './Footer';
+import Navbar from './Navbar';
+import Sidebar from './Sidebar';
+
+export { CategorySidebar, CheckoutSidebar, Footer, Navbar, Sidebar };
