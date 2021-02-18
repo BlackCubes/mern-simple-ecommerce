923ccf1d10eedfbe9c9c7d63fc452ef188978fc8
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-18 06:57:56 -0800

    Created custom form styled components

---
+++ src/common/FormStyled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const FormStyled = styled.form`
+  width: 40rem;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const FormGroupStyled = styled.div`
+  margin: 1.5rem 0;
+`;
