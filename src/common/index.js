4fa05d1e05ae8f47fe038fd8a073d08a0baa0354
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-17 19:33:40 -0800

    Refactored

--- src/common/index.js
+++ src/common/index.js
@@ -11,7 +11,7 @@ import HeadingQuaternary from './HeadingQuaternary';
 import HeadingSecondary from './HeadingSecondary';
 import HeadingTertiary from './HeadingTertiary';
 import Image from './Image';
-import Input from './Input';
+import * as InputStyled from './Input';
 import Link from './Link';
 import * as NavbarStyled from './NavbarStyled';
 import Paragraph from './Paragraph';
@@ -35,7 +35,7 @@ export {
   HeadingSecondary,
   HeadingTertiary,
   Image,
-  Input,
+  InputStyled,
   Link,
   NavbarStyled,
   Paragraph,
