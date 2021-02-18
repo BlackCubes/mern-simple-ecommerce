397083594a5d1bb6a3bad0f3b06ba4a6643aa1df
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-18 06:24:51 -0800

    Refactored

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,7 +3,6 @@ import CheckoutSidebar from './CheckoutSidebar';
 import Footer from './Footer';
 import Input from './Input';
 import Modal from './Modal';
-// import Modal from './Modal';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 
@@ -13,7 +12,6 @@ export {
   Footer,
   Input,
   Modal,
-  // Modal,
   Navbar,
   Sidebar,
 };
