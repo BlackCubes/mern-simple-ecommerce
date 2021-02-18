d650ec2ea646e58b2f07de90faf05ed369714266
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-17 19:39:09 -0800

    Changed the import and export

--- src/common/index.js
+++ src/common/index.js
@@ -11,7 +11,7 @@ import HeadingQuaternary from './HeadingQuaternary';
 import HeadingSecondary from './HeadingSecondary';
 import HeadingTertiary from './HeadingTertiary';
 import Image from './Image';
-import * as InputStyled from './InputStyled';
+import { LabelStyled, InputStyled, InputMessageStyled } from './InputStyled';
 import Link from './Link';
 import * as NavbarStyled from './NavbarStyled';
 import Paragraph from './Paragraph';
@@ -35,7 +35,9 @@ export {
   HeadingSecondary,
   HeadingTertiary,
   Image,
+  InputMessageStyled,
   InputStyled,
+  LabelStyled,
   Link,
   NavbarStyled,
   Paragraph,
