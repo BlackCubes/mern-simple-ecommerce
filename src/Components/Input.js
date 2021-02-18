cd466618c70a4eee275eb71db725ce3cdad78b77
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-17 22:32:48 -0800

    Created an input component

---
+++ src/Components/Input.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { InputMessageStyled, InputStyled, LabelStyled } from '../common';
+
+const Input = ({ inputprop, error }) => (
+  <LabelStyled htmlFor={inputprop.id}>
+    <InputStyled
+      type={inputprop.type}
+      name={inputprop.name}
+      className={inputprop.className}
+      id={inputprop.id}
+      value={inputprop.value}
+      placeholder={inputprop.placeholder}
+      onChange={inputprop.onChange}
+      noValidate={inputprop.noValidate}
+    />
+
+    <InputMessageStyled>{error || inputprop.message}</InputMessageStyled>
+  </LabelStyled>
+);
+
+Input.propTypes = {
+  inputprop: PropTypes.shape({
+    type: PropTypes.string,
+    name: PropTypes.string.isRequired,
+    className: PropTypes.string,
+    id: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    placeholder: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    noValidate: PropTypes.bool,
+    message: PropTypes.string.isRequired,
+  }).isRequired,
+  error: PropTypes.string,
+};
+
+Input.defaultProps = { error: null };
+
+export default Input;
