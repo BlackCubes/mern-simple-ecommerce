604e2edfe3dd88125b1d98069c6b92d7256d0dfe
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-18 00:45:41 -0800

    Refactored

--- src/providers/CartProvider.js
+++ src/providers/CartProvider.js
@@ -7,20 +7,6 @@ const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
   console.log('cart: ', cart);
 
-  // const cartQuantity = (items, newItem) => {
-  //   if (!items.length) return [{ ...newItem, quantity: 1 }];
-
-  //   let itemsObj = {};
-
-  //   items.forEach((item) => {
-  //     if (item.id !== newItem.id) {
-  //       itemsObj = { ...newItem, quantity: 1 };
-  //       items.push(itemsObj);
-  //     }
-  //   });
-
-  //   return items;
-  // };
   const addToCart = (items, newItem) => {
     const existingItem = items.find((item) => item.id === newItem.id);
     if (existingItem)
