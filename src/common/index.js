2fce95622fb338d9844975eb30c288f576c2da57
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-17 23:38:48 -0800

    Refactored

--- src/common/index.js
+++ src/common/index.js
@@ -1,49 +1,6 @@
 import Button from './Button';
-import * as CartStyled from './CartStyled';
-import * as CategorySidebarStyled from './CategorySidebarStyled';
-import * as CheckoutSidebarStyled from './CheckoutSidebarStyled';
-import * as CheckoutStyled from './CheckoutStyled';
-import * as FooterStyled from './FooterStyled';
-// import Form from './Form';
-import GlobalStyles from './GlobalStyles';
-import HeadingPrimary from './HeadingPrimary';
-import HeadingQuaternary from './HeadingQuaternary';
-import HeadingSecondary from './HeadingSecondary';
-import HeadingTertiary from './HeadingTertiary';
 import Image from './Image';
 import { LabelStyled, InputStyled, InputMessageStyled } from './InputStyled';
 import Link from './Link';
-import * as NavbarStyled from './NavbarStyled';
-import Paragraph from './Paragraph';
-import * as ProductDetailsStyled from './ProductDetailsStyled';
-import * as ProductsStyled from './ProductsStyled';
-import * as SidebarStyled from './SidebarStyled';
-import Small from './Small';
-import Theme from './theme';
 
-export {
-  Button,
-  CartStyled,
-  CategorySidebarStyled,
-  CheckoutSidebarStyled,
-  CheckoutStyled,
-  FooterStyled,
-  // Form,
-  GlobalStyles,
-  HeadingPrimary,
-  HeadingQuaternary,
-  HeadingSecondary,
-  HeadingTertiary,
-  Image,
-  InputMessageStyled,
-  InputStyled,
-  LabelStyled,
-  Link,
-  NavbarStyled,
-  Paragraph,
-  ProductDetailsStyled,
-  ProductsStyled,
-  SidebarStyled,
-  Small,
-  Theme,
-};
+export { Button, Image, InputMessageStyled, InputStyled, LabelStyled, Link };
