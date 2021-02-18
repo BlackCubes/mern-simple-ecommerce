90f6a19226afd1f62f4cb286849ec86725867a5f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-17 23:07:37 -0800

    Imported and exported modules

---
+++ src/common/Sidebar/index.js
@@ -0,0 +1,49 @@
+import {
+  CategorySidebarStyled,
+  CategorySidebarHeaderStyled,
+  CategorySidebarCardStyled,
+  CategorySidebarCardImageStyled,
+  CategorySidebarCardInfoStyled,
+  CategorySidebarCardInfoTitleStyled,
+  CategorySidebarCardInfoPriceStyled,
+} from './CategorySidebarStyled';
+import {
+  CheckoutSidebarStyled,
+  CheckoutSidebarHeaderStyled,
+  CheckoutSidebarSummaryStyled,
+  CheckoutSidebarSummaryLineStyled,
+  CheckoutSidebarSummaryTotalStyled,
+  CheckoutSidebarButton,
+} from './CheckoutSidebarStyled';
+import {
+  SidebarStyled,
+  SidebarContainerStyled,
+  SidebarContentStyled,
+  SidebarFacetStyled,
+  SidebarFacetLegendStyled,
+  SidebarFacetListStyled,
+  SidebarFacetListItem,
+} from './SidebarStyled';
+
+export {
+  CategorySidebarStyled,
+  CategorySidebarHeaderStyled,
+  CategorySidebarCardStyled,
+  CategorySidebarCardImageStyled,
+  CategorySidebarCardInfoStyled,
+  CategorySidebarCardInfoTitleStyled,
+  CategorySidebarCardInfoPriceStyled,
+  CheckoutSidebarStyled,
+  CheckoutSidebarHeaderStyled,
+  CheckoutSidebarSummaryStyled,
+  CheckoutSidebarSummaryLineStyled,
+  CheckoutSidebarSummaryTotalStyled,
+  CheckoutSidebarButton,
+  SidebarStyled,
+  SidebarContainerStyled,
+  SidebarContentStyled,
+  SidebarFacetStyled,
+  SidebarFacetLegendStyled,
+  SidebarFacetListStyled,
+  SidebarFacetListItem,
+};
