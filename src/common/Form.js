d49cbe44bcc49979c68cf8f33277001d7572dc56
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-17 18:06:40 -0800

    Implemented utility functions, and testing it

--- src/common/Form.js
+++ src/common/Form.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 // import styled from 'styled-components';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
+
+import { fieldInputErrors, fieldInputProperties } from '../utils';
 
 const Form = ({ onSubmit, formFields, InputComponent }) => {
   const [values, setValues] = useState({});
@@ -19,9 +21,29 @@ const Form = ({ onSubmit, formFields, InputComponent }) => {
     onSubmit(values);
   };
 
+  const inputProperties = formFields.map((prop) =>
+    fieldInputProperties({
+      type: prop.type,
+      name: prop.name,
+      errors: prop.errors,
+      values: prop.values,
+      placeholder: prop.placeholder,
+      onChange: handleChange,
+      noValidate: prop.noValidate,
+    })
+  );
+
+  const inputErrors = formFields.map((prop) =>
+    fieldInputErrors(prop.name, errors)
+  );
+
+  console.log('inputProperties: ', inputProperties);
+  console.log('inputErrors: ', inputErrors);
+  console.log('InputComponent: ', InputComponent);
+
   return (
     <form onSubmit={handleSubmit} noValidate>
-      {formFields.map((prop) => (
+      {/* {formFields.map((prop) => (
         <InputComponent
           key={uuidv4()}
           inputprop={prop}
@@ -29,7 +51,7 @@ const Form = ({ onSubmit, formFields, InputComponent }) => {
           errors={errors}
           onChange={handleChange}
         />
-      ))}
+      ))} */}
     </form>
   );
 };
