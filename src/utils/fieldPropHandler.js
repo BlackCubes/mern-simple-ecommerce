ffb55237e1ab408b8546b60a182182714460fa34
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-17 17:45:09 -0800

    Created a field input/error handler functions

---
+++ src/utils/fieldPropHandler.js
@@ -0,0 +1,12 @@
+export const fieldInputProperties = (prop) => ({
+  type: prop.type || 'text',
+  name: prop.name,
+  className: prop.errors[prop.name] && 'error',
+  value: prop.values[prop.name] || '',
+  placeholder: prop.placeholder,
+  onChange: prop.onChange,
+  noValidate: prop.noValidate || true,
+});
+
+export const fieldInputErrors = (name, errors) =>
+  errors[name] ? errors[name] : null;
