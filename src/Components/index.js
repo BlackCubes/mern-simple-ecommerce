bde3c2b4fec5f4f076abc42d2762fcb1dda36b2d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-18 01:55:09 -0800

    Imported and exported module

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,7 +2,16 @@ import CategorySidebar from './CategorySidebar';
 import CheckoutSidebar from './CheckoutSidebar';
 import Footer from './Footer';
 import Input from './Input';
+import Modal from './Modal';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 
-export { CategorySidebar, CheckoutSidebar, Footer, Input, Navbar, Sidebar };
+export {
+  CategorySidebar,
+  CheckoutSidebar,
+  Footer,
+  Input,
+  Modal,
+  Navbar,
+  Sidebar,
+};
