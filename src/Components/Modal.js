d79021bafe174f85d9f7ef528199c75a8df488f5
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-18 01:54:10 -0800

    Created a base modal

---
+++ src/Components/Modal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Modal = ({ children, header }) => (
+  <div className="modal">
+    <div>{header}</div>
+
+    <div>{children}</div>
+  </div>
+);
+
+Modal.propTypes = {
+  children: PropTypes.node.isRequired,
+  header: PropTypes.string.isRequired,
+};
+
+export defailt Modal;
