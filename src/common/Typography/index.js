e1705d62f2a5db879c3131292989e49c50ef2bcb
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-17 22:52:24 -0800

    Imported and exported modules

---
+++ src/common/Typography/index.js
@@ -0,0 +1,15 @@
+import HeadingPrimary from './HeadingPrimary';
+import HeadingQuarternary from './HeadingQuaternary';
+import HeadingSecondary from './HeadingSecondary';
+import HeadingTertiary from './HeadingTertiary';
+import Paragraph from './Paragraph';
+import Small from './Small';
+
+export {
+  HeadingPrimary,
+  HeadingQuarternary,
+  HeadingSecondary,
+  HeadingTertiary,
+  Paragraph,
+  Small,
+};
