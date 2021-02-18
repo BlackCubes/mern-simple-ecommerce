57323323ab23c869ac65ea52be6d3cd43575f548
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-18 06:44:19 -0800

    Imported and exported modal styled components

--- src/common/index.js
+++ src/common/index.js
@@ -2,5 +2,22 @@ import Button from './Button';
 import Image from './Image';
 import { LabelStyled, InputStyled, InputMessageStyled } from './InputStyled';
 import Link from './Link';
+import {
+  ModalStyled,
+  ModalHeaderStyled,
+  ModalBodyStyled,
+  ModalButtonStyled,
+} from './ModalStyled';
 
-export { Button, Image, InputMessageStyled, InputStyled, LabelStyled, Link };
+export {
+  Button,
+  Image,
+  InputMessageStyled,
+  InputStyled,
+  LabelStyled,
+  Link,
+  ModalStyled,
+  ModalHeaderStyled,
+  ModalBodyStyled,
+  ModalButtonStyled,
+};
