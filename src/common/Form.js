2fa77df669ac4f04eb381fc142b0f3b7e5466369
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-17 19:33:04 -0800

    Refactored

--- src/common/Form.js
+++ src/common/Form.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 // import styled from 'styled-components';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
 
 import { fieldInputErrors, fieldInputProperties } from '../utils';
 
