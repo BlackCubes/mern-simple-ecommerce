477193acd0fa1491f503630ff19ea452cfff602a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-18 00:25:17 -0800

    Imported and exported module

---
+++ src/containers/index.js
@@ -0,0 +1,13 @@
+import CartPage from './CartPage';
+import CheckoutPage from './CheckoutPage';
+import FormContainer from './FormContainer';
+import ProductDetailPage from './ProductDetailsPage';
+import ProductsPage from './ProductsPage';
+
+export {
+  CartPage,
+  CheckoutPage,
+  FormContainer,
+  ProductDetailPage,
+  ProductsPage,
+};
