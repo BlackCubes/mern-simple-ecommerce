01936a66492a4f29566d1543d937fa7295264a53
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-20 13:33:37 -0800

    Created two new styled components, and refactored

--- src/common/ModalStyled.js
+++ src/common/ModalStyled.js
@@ -1,17 +1,22 @@
 import styled from 'styled-components';
 
-export const ModalStyled = styled.div`
+export const ModalOverlayStyled = styled.div`
   position: fixed;
-  width: 50%;
-  padding: 3.6rem;
-  background-color: ${(props) => props.theme.colors.white};
-  border: 0.1rem solid #ccc;
-  box-shadow: 0.1rem 0.1rem 0.1rem ${(props) => props.theme.colors.black};
-  left: 15%;
-  top: 30%;
-  box-sizing: border-box;
-  transition: all 0.3s ease-out;
-  z-index: 500;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  z-index: 2000;
+`;
+
+export const ModalWindowStyled = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(29, 37, 44, 0.65);
+  opacity: ${({ modaltoggle }) => (modaltoggle ? 1 : 0)};
+  overflow-y: scroll;
+  overflow-x: hidden;
+  touch-action: pan-y;
 `;
 
 export const ModalContainerStyled = styled.div`
@@ -20,10 +25,40 @@ export const ModalContainerStyled = styled.div`
   margin-right: auto;
   padding-left: 1.5rem;
   padding-right: 1.5rem;
-  transform: ${({ modaltoggle }) =>
-    modaltoggle ? 'translateY(0)' : 'translateY(-100vh)'};
-  opacity: ${({ modaltoggle }) => (modaltoggle ? 1 : 0)};
 `;
+
+export const ModalStyled = styled.div`
+  width: 50%;
+  position: relative;
+  margin: 6rem auto;
+  padding: 3.6rem;
+  background-color: ${(props) => props.theme.colors.white};
+`;
+
+// export const ModalContainerStyled = styled.div`
+//   width: 117rem;
+//   margin-left: auto;
+//   margin-right: auto;
+//   padding-left: 1.5rem;
+//   padding-right: 1.5rem;
+//   transform: ${({ modaltoggle }) =>
+//     modaltoggle ? 'translateY(0)' : 'translateY(-100vh)'};
+//   opacity: ${({ modaltoggle }) => (modaltoggle ? 1 : 0)};
+// `;
+
+// export const ModalStyled = styled.div`
+//   position: fixed;
+//   width: 50%;
+//   padding: 3.6rem;
+//   background-color: ${(props) => props.theme.colors.white};
+//   border: 0.1rem solid #ccc;
+//   box-shadow: 0.1rem 0.1rem 0.1rem ${(props) => props.theme.colors.black};
+//   left: 15%;
+//   top: 30%;
+//   box-sizing: border-box;
+//   transition: all 0.3s ease-out;
+//   z-index: 500;
+// `;
 
 export const ModalHeaderStyled = styled.div`
   margin-bottom: 1rem;
