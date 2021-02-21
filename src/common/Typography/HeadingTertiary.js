c6c410b44c3121a4535518a86e8eb3e08280a935
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-20 17:04:37 -0800

    Added fontweighttype prop

--- src/common/Typography/HeadingTertiary.js
+++ src/common/Typography/HeadingTertiary.js
@@ -4,15 +4,20 @@ import styled from 'styled-components';
 
 const HeadingTertiaryStyled = styled.h3`
   font-size: 2.5rem;
-  font-weight: 400;
+  font-weight: ${({ fontweighttype }) => fontweighttype || 400};
 `;
 
-const HeadingTertiary = ({ children }) => (
-  <HeadingTertiaryStyled>{children}</HeadingTertiaryStyled>
+const HeadingTertiary = ({ children, fontweighttype }) => (
+  <HeadingTertiaryStyled fontweighttype={fontweighttype}>
+    {children}
+  </HeadingTertiaryStyled>
 );
 
 HeadingTertiary.propTypes = {
   children: PropTypes.node.isRequired,
+  fontweighttype: PropTypes.string,
 };
+
+HeadingTertiary.defaultProps = { fontweighttype: 400 };
 
 export default HeadingTertiary;
