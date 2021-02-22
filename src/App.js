b88334f523507723226dbcfdbd8f92ba5fac1f6f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-22 12:45:22 -0800

    Imported react-credit-cards styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import logo from './logo.svg';
 // import './App.css';
+import 'react-credit-cards/es/styles-compiled.css';
 
 import { Footer, Navbar } from './Components';
 
