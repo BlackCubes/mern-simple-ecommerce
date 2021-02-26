b0d5e1b071cdfbe9f001a74a3106c9f7ae0ef6ca
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-26 12:10:37 -0800

    Imported: React and PropTypes dependencies; Changed: export default into styled component function; Created/propTypes/Default-Export: component function with styled components and React

--- client/src/common/Base/MainStyled.js
+++ client/src/common/Base/MainStyled.js
@@ -1,6 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-export default styled.main`
+const MainStyled = styled.main`
   margin-top: 2rem;
   margin-bottom: 2rem;
 `;
+
+const Main = ({ children }) => <MainStyled>{children}</MainStyled>;
+
+Main.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Main;
