f5858ef76385ebf73f7f8addb476962d43e8e3b9
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-27 11:45:22 -0800

    Backend Setup: Security features for node backend

--- api/app.js
+++ api/app.js
@@ -1,5 +1,46 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const cookieParser = require('cookie-parser');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const app = express();
+
+// Implement CORS
+app.use(cors());
+app.options('*', cors());
+
+// GLOBAL MIDDLEWARES
+// Helmet -- set security HTTP headers
+app.use(helmet());
+
+// Morgan -- development logging
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
+// Express-Rate-Limit --  limit requests from some API
+const limiter = rateLimit({
+  max: 20,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP. Please try again in an hour.',
+});
+app.use('/api', limiter);
+
+// Body Parser -- reading data from the body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ limit: '10kb', extended: true }));
+app.use(cookieParser());
+
+// Data Sanitization
+// --- against NoSQL query injection
+app.use(mongoSanitize());
+
+// -- against XSS
+app.use(xss());
+
+// HPP -- prevent parameter pollution
+// nothing yet
 
 module.exports = app;
