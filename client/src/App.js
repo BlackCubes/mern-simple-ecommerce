259e9aa8576871fd758c125c2dcd9f95146a2c76
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-27 08:44:00 -0800

    Imported/Implemented: Main component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import './App.css';
 import 'react-credit-cards/es/styles-compiled.css';
 
+import { Main } from './common/Base';
+
 import { Footer, Navbar } from './Components';
 
 import {
@@ -23,21 +25,23 @@ function App() {
         <CartProvider>
           <CheckoutProvider>
             <Navbar />
-            <Switch>
-              <Route exact path="/" render={() => <div>Homepage</div>} />
-              <Route exact path="/products" component={ProductsPage} />
-              <Route
-                path="/products/category/:category"
-                component={ProductsPage}
-              />
-              <Route exact path="/products/:id">
-                <ProductDetailsPage FormContainerComponent={FormContainer} />
-              </Route>
-              <Route exact path="/cart" component={CartPage} />
-              <Route exact path="/checkout">
-                <CheckoutPage FormContainerComponent={FormContainer} />
-              </Route>
-            </Switch>
+            <Main>
+              <Switch>
+                <Route exact path="/" render={() => <div>Homepage</div>} />
+                <Route exact path="/products" component={ProductsPage} />
+                <Route
+                  path="/products/category/:category"
+                  component={ProductsPage}
+                />
+                <Route exact path="/products/:id">
+                  <ProductDetailsPage FormContainerComponent={FormContainer} />
+                </Route>
+                <Route exact path="/cart" component={CartPage} />
+                <Route exact path="/checkout">
+                  <CheckoutPage FormContainerComponent={FormContainer} />
+                </Route>
+              </Switch>
+            </Main>
             <Footer />
           </CheckoutProvider>
         </CartProvider>
