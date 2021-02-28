a860dcdd71490205b807104a8bf07e60a2b0f008
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-28 12:02:31 -0800

    Removed/Changed: Review model and transformed it into string

--- api/controllers/reviewController.js
+++ api/controllers/reviewController.js
@@ -1,8 +1,7 @@
 const factory = require('./handlerFactory');
-const { Review } = require('../models');
 
-exports.getAllReviews = factory.getAll(Review);
-exports.getReview = factory.getOne(Review);
-exports.createReview = factory.createOne(Review);
-exports.updateReview = factory.updateOne(Review);
-exports.deleteReview = factory.deleteOne(Review);
+exports.getAllReviews = factory.getAll('Review');
+exports.getReview = factory.getOne('Review');
+exports.createReview = factory.createOne('Review');
+exports.updateReview = factory.updateOne('Review');
+exports.deleteReview = factory.deleteOne('Review');
