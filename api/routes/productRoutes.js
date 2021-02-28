05abec40b5bd304f4477ff7699589c7a6b203832
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-28 11:52:52 -0800

    Imported: product controller middleware. Created: product routes for getAll, getOne, post, patch, and delete

--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -1,9 +1,21 @@
 const express = require('express');
 
 const reviewRouter = require('./reviewRoutes');
+const { productController } = require('../controllers');
 
 const router = express.Router();
 
 router.use('/:productId/reviews', reviewRouter);
+
+router
+  .route('/')
+  .get(productController.getAllProducts)
+  .post(productController.createProduct);
+
+router
+  .router('/:id')
+  .get(productController.getProduct)
+  .patch(productController.updateProduct)
+  .delete(productController.deleteProduct);
 
 module.exports = router;
