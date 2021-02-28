ad6871f9019bbbf0088325a00aa42ff814b02655
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-27 19:56:07 -0800

    Imported: handler factory, and Review model; Created/Exported: get all, get one, create one, update one, and delete one middlewares

---
+++ api/controllers/reviewController.js
@@ -0,0 +1,8 @@
+const factory = require('./handlerFactory');
+const { Review } = require('../models');
+
+exports.getAllReviews = factory.getAll(Review);
+exports.getReview = factory.getOne(Review);
+exports.createReview = factory.createOne(Review);
+exports.updateReview = factory.updateOne(Review);
+exports.deleteReview = factory.deleteOne(Review);
