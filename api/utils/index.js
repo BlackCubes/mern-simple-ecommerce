f6233314d72e8be2d1f6bf36e92ee1ae17b25c68
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-28 10:23:23 -0800

    Imported/Exported: sanitize

--- api/utils/index.js
+++ api/utils/index.js
@@ -2,5 +2,6 @@ const AppError = require('./appError');
 const capitalize = require('./capitalize');
 const catchAsync = require('./catchAsync');
 const filterObj = require('./filterObject');
+const sanitize = require('./sanitize');
 
-module.exports = { AppError, capitalize, catchAsync, filterObj };
+module.exports = { AppError, capitalize, catchAsync, filterObj, sanitize };
