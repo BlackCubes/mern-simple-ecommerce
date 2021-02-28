7fdf3a21f9206fcebb1e9e7dafcfaaceb8cab163
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-28 10:12:20 -0800

    Fixed: moved custom filter function from userController.js to proper file in filterObject.js

--- api/controllers/userController.js
+++
@@ -1,7 +0,0 @@
-module.exports = (obj, ...allowedFields) => {
-  const newObj = {};
-  Object.keys(obj).forEach((el) => {
-    if (allowedFields.includes(el)) newObj[el] = obj[el];
-  });
-  return newObj;
-};
