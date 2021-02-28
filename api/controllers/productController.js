8315fa9b135afcd7d5df263ee811345108f20aff
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-28 11:46:31 -0800

    Created/Exported: getAll, get, create, update, and delete middlewares showing that the route is not defined, and to refer to the Fake Store API documentation for more info

---
+++ api/controllers/productController.js
@@ -0,0 +1,35 @@
+exports.getAllProducts = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message:
+      'This route is not defined! Please refer to the Fake Store API documentation on getting all products: https://fakestoreapi.com/docs.',
+  });
+};
+exports.getProduct = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message:
+      'This route is not defined! Please refer to the Fake Store API documentation on getting a single product: https://fakestoreapi.com/docs.',
+  });
+};
+exports.createProduct = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message:
+      'This route is not defined! Please refer to the Fake Store API documentation on adding a new product: https://fakestoreapi.com/docs.',
+  });
+};
+exports.updateProduct = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message:
+      'This route is not defined! Please refer to the Fake Store API documentation on updating a product: https://fakestoreapi.com/docs.',
+  });
+};
+exports.deleteProduct = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message:
+      'This route is not defined! Please refer to the Fake Store API documentation on deleting a product: https://fakestoreapi.com/docs.',
+  });
+};
