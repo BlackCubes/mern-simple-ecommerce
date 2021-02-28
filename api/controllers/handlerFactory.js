936ee39ce3db8cdc818d3e4a6b52a522381e6c03
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-27 18:51:05 -0800

    Imported: Models object, custom AppError function, and catchAsync function; Created/Exported: create one, get all, get one, update one, and delete one handler middleware controller functions

---
+++ api/controllers/handlerFactory.js
@@ -0,0 +1,73 @@
+const Models = require('./../models');
+const { AppError, catchAsync } = require('./../utils');
+
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Models[Model].create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Models[Model].find();
+
+    res.status(200).json({
+      status: 'success',
+      results: doc.length,
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.getOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Models[Model].findById(req.params.id);
+
+    if (!doc)
+      return next(new AppError(`There is no ${Model} with that ID! 😰`, 404));
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Models[Model].findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc)
+      return next(new AppError(`There is no ${Model} with that ID! 😰`, 404));
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+exports.deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Models[Model].findByIdAndDelete(req.params.id);
+
+    if (!doc)
+      return next(new AppError(`There is no ${Model} with that ID! 😰`, 404));
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  });
