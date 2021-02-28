6a8a3c6be6d796d850af747cac9d4b425c35e314
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-28 11:47:41 -0800

    Imported/Exported: product controller middleware

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,5 +1,11 @@
 const globalErrorHandler = require('./errorController');
 const reviewController = require('./reviewController');
+const productController = require('./productController');
 const userController = require('./userController');
 
-module.exports = { globalErrorHandler, reviewController, userController };
+module.exports = {
+  globalErrorHandler,
+  reviewController,
+  productController,
+  userController,
+};
