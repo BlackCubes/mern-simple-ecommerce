0ee69e258f70b82503ca1e9d1e7e621dd6cba2dc
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-01 08:45:10 -0800

    Imported/Exported: Email class

--- api/utils/index.js
+++ api/utils/index.js
@@ -1,7 +1,15 @@
 const AppError = require('./appError');
 const capitalize = require('./capitalize');
 const catchAsync = require('./catchAsync');
+const Email = require('./email');
 const filterObj = require('./filterObject');
 const sanitize = require('./sanitize');
 
-module.exports = { AppError, capitalize, catchAsync, filterObj, sanitize };
+module.exports = {
+  AppError,
+  capitalize,
+  catchAsync,
+  Email,
+  filterObj,
+  sanitize,
+};
