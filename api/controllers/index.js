327ff7677d357dcfc2da103c89d9284675d8769b
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-02-28 18:09:56 -0800

    Imported/Exported: authentication controller middleware

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,9 +1,11 @@
+const authController = require('./authController');
 const globalErrorHandler = require('./errorController');
 const reviewController = require('./reviewController');
 const productController = require('./productController');
 const userController = require('./userController');
 
 module.exports = {
+  authController,
   globalErrorHandler,
   reviewController,
   productController,
