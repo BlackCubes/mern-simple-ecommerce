7d2bad7265d437e2fe6a264897d67f6aade7a5d5
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-01 22:00:22 -0800

    Imported: validator handler, AppError utility class, and catchAsync utility function. Created: errMsg function to display each errors given an error object, a refactored givenValidation function to be reused with the handler validator, a signup validation middleware to validate users signing up, a login validation middleware to validate users logging in, a forgotPassword validation middleware to validate users that forgot their passwords, a resetPassword validation middleware to validate users that are resetting their password, an updatePassword validation middleware to validate users updating their password, updateUser validation middleware to validate users updating their account, a createReview validation middleware to validate users creating a review, and an updateReview validation middlware to validate users/admins updating reviews. Exported Validation Middlewares: signup, login, forgotPassword, resetPassword, updatePassword, updateUser, createReview, and updateReview

---
+++ api/validation/validationController.js
@@ -0,0 +1,93 @@
+const validator = require('./validate');
+
+const { AppError, catchAsync } = require('../utils');
+
+const errMsg = (errObj) => {
+  let message = '';
+  Object.values(errObj).forEach((err) => (message += `${err[0]} `));
+  return message.slice(0, -1);
+};
+
+const givenValidation = (req, next, validationRule) =>
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) return next(new AppError(`${errMsg(err.errors)}`, 400));
+    next();
+  });
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const validationRule = {
+    name: 'required|string|min:2|max:70|regexName',
+    email: 'required|email|exist:User,email',
+    password: 'required|string|min:8|max:60|confirmed|regexPass',
+    password_confirmation: 'required|string',
+  };
+
+  givenValidation(req, next, validationRule);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const validationRule = {
+    email: 'required|email',
+    password: 'required|string|min:8|max:60|regexPass',
+  };
+
+  givenValidation(req, next, validationRule);
+});
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  const validationRule = {
+    email: 'required|email',
+  };
+
+  givenValidation(req, next, validationRule);
+});
+
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  const validationRule = {
+    password: 'required|string|min:8|max:60|confirmed|regexPass',
+    password_confirmation: 'required|string',
+  };
+
+  givenValidation(req, next, validationRule);
+});
+
+exports.updatePassword = catchAsync(async (req, res, next) => {
+  const validationRule = {
+    password_current: 'required|string|min:8|max:60|regexPass',
+    password: 'required|string|min:8|max:60|confirmed|regexPass',
+    password_confirmation: 'required|string',
+  };
+
+  givenValidation(req, next, validationRule);
+});
+
+exports.updateUser = catchAsync(async (req, res, next) => {
+  const validationRule = {
+    name: 'string|max:70|regexName',
+    email: 'email',
+  };
+
+  givenValidation(req, next, validationRule);
+});
+
+exports.createReview = catchAsync(async (req, res, next) => {
+  const validationRule = {
+    review: 'required|string|min:20|max:280',
+    rating: 'required|numeric|min:1|max:5|regexRating',
+    userfullname: 'required|string|min:2|max:70|regexName',
+    productId: 'required|numeric|min:1',
+  };
+
+  givenValidation(req, next, validationRule);
+});
+
+exports.updateReview = catchAsync(async (req, res, next) => {
+  const validationRule = {
+    review: 'string|min:20|max:280',
+    rating: 'numeric|min:1|max:5|regexRating',
+    userfullname: 'string|min:2|max:70|regexName',
+    productId: 'required|numeric|min:1',
+  };
+
+  givenValidation(req, next, validationRule);
+});
