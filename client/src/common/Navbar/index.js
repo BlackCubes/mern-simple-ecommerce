66cf4c82af30fe9435a199dafacee84c604d6149
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-02 12:43:49 -0800

    Imported/Exported: all navbar styled components

---
+++ client/src/common/Navbar/index.js
@@ -0,0 +1,27 @@
+import {
+  NavbarStyled,
+  NavbarContainerStyled,
+  NavbarLogoStyled,
+  NavbarMenuHolderStyled,
+  NavbarMenuStyled,
+  NavbarMenuListStyled,
+  NavbarMenuListItemStyled,
+  NavbarCartStyled,
+  NavbarCartImageStyled,
+  NavbarCartLabelStyled,
+  NavbarCartQuantityStyled,
+} from './NavbarStyled';
+
+export {
+  NavbarStyled,
+  NavbarContainerStyled,
+  NavbarLogoStyled,
+  NavbarMenuHolderStyled,
+  NavbarMenuStyled,
+  NavbarMenuListStyled,
+  NavbarMenuListItemStyled,
+  NavbarCartStyled,
+  NavbarCartImageStyled,
+  NavbarCartLabelStyled,
+  NavbarCartQuantityStyled,
+};
