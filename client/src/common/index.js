acf518435fe91ee2d70d16dba4957377cdba444e
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-03 13:09:39 -0800

    Removed: imports and exports of the original input custom styled components

--- client/src/common/index.js
+++ client/src/common/index.js
@@ -2,7 +2,6 @@ import Button from './Button';
 import CreditCardStyled from './CreditCardStyled';
 import { FormStyled, FormGroupStyled } from './FormStyled';
 import Image from './Image';
-import { LabelStyled, InputStyled, InputMessageStyled } from './InputStyled';
 import Link from './Link';
 import {
   ModalOverlayStyled,
@@ -20,9 +19,6 @@ export {
   FormStyled,
   FormGroupStyled,
   Image,
-  InputMessageStyled,
-  InputStyled,
-  LabelStyled,
   Link,
   ModalOverlayStyled,
   ModalWindowStyled,
