64ccd35239d8aeb0916bfadc46c8ce601e9706a1
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-02 17:26:11 -0800

    Removed: custom footer styled components

--- client/src/common/Base/index.js
+++ client/src/common/Base/index.js
@@ -1,29 +1,5 @@
-import {
-  FooterStyled,
-  FooterFollowStyled,
-  FooterFollowHeaderStyled,
-  FooterFollowNavigationStyled,
-  FooterFollowListStyled,
-  FooterFollowItemStyled,
-  FooterLowerStyled,
-  FooterCopyrightStyled,
-  FooterCopyrightContentStyled,
-} from './FooterStyled';
 import GlobalStyles from './GlobalStyles';
 import Main from './Main';
 import Theme from './Theme';
 
-export {
-  FooterStyled,
-  FooterFollowStyled,
-  FooterFollowHeaderStyled,
-  FooterFollowNavigationStyled,
-  FooterFollowListStyled,
-  FooterFollowItemStyled,
-  FooterLowerStyled,
-  FooterCopyrightStyled,
-  FooterCopyrightContentStyled,
-  GlobalStyles,
-  Main,
-  Theme,
-};
+export { GlobalStyles, Main, Theme };
