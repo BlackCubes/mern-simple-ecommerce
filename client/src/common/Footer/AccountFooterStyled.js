41885f0973217ab34919d14ea7243239a6c9d37e
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-02 18:16:10 -0800

    Imported: styled-components module. Exported/Created: custom account footer styled components

---
+++ client/src/common/Footer/AccountFooterStyled.js
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+export const AccountFooterStyled = styled.footer`
+  position: absolute;
+  font-family: inherit;
+  width: 100%;
+  height: 16rem;
+  background-color: ${(props) => props.theme.colors.black_gray};
+  bottom: 0;
+`;
+
+export const AccountFooterCreatedStyled = styled.div`
+  height: 10rem;
+`;
+
+export const AccountFooterCreatedContainerStyled = styled.div`
+  padding: 2rem;
+  text-align: center;
+  list-style: none;
+`;
+
+export const AccountFooterCreatedTitleStyled = styled.div`
+  color: #546c84;
+  padding-bottom: 1rem;
+
+  & span {
+    font-size: 1.5rem;
+  }
+`;
+
+export const AccountFooterCreatedNavigationStyled = styled.div`
+  padding: 1.3rem;
+`;
+
+export const AccountFooterCreatedListStyled = styled.ul`
+  margin: 0;
+  padding: 0;
+`;
+
+export const AccountFooterCreatedItemStyled = styled.li`
+  display: inline-block;
+  font-size: 1.2rem;
+
+  & a {
+    color: #546c84;
+  }
+
+  &:nth-child(2)::before {
+    content: ' | ';
+    margin: 0 0.8rem;
+  }
+`;
