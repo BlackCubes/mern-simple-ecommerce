1fc12139a4eae3bd2ff7b982372da1e8d650cac3
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-03 13:03:11 -0800

    Created/Exported: input messages custom styled component. Imported Module: styled-components

---
+++ client/src/common/Inputs/InputMessageStyled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export default styled.span`
+  display: block;
+  color: ${(props) => props.theme.colors.moderate_blue_2};
+  margin-top: 0.5rem;
+  text-align: left;
+  transition: all 0.3s;
+`;
