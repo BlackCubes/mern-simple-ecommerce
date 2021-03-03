62893f7288ed83572067b2eba8dd7c5b4a275ec3
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-03 05:43:26 -0800

    Removed: AccountNavbar and Navbar components

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -1,21 +1,17 @@
-import AccountFooter from './AccountFooter';
 import AccountNavbar from './AccountNavbar';
 import CategorySidebar from './CategorySidebar';
 import CheckoutSidebar from './CheckoutSidebar';
 import CreditCard from './CreditCard';
-import Footer from './Footer';
 import Input from './Input';
 import Modal from './Modal';
 import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 
 export {
-  AccountFooter,
   AccountNavbar,
   CategorySidebar,
   CheckoutSidebar,
   CreditCard,
-  Footer,
   Input,
   Modal,
   Navbar,
