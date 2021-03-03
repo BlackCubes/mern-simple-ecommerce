0845bb140bedddf84ddd946c23d20a3c9c05dfd5
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-02 17:24:42 -0800

    Imported/Exported: custom footer styled components

---
+++ client/src/common/Footer/index.js
@@ -0,0 +1,23 @@
+import {
+  FooterStyled,
+  FooterFollowStyled,
+  FooterFollowHeaderStyled,
+  FooterFollowNavigationStyled,
+  FooterFollowListStyled,
+  FooterFollowItemStyled,
+  FooterLowerStyled,
+  FooterCopyrightStyled,
+  FooterCopyrightContentStyled,
+} from './FooterStyled';
+
+export {
+  FooterStyled,
+  FooterFollowStyled,
+  FooterFollowHeaderStyled,
+  FooterFollowNavigationStyled,
+  FooterFollowListStyled,
+  FooterFollowItemStyled,
+  FooterLowerStyled,
+  FooterCopyrightStyled,
+  FooterCopyrightContentStyled,
+};
