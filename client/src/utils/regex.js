55ed2ad967da3bf80217b3586fbaf867ae47dda0
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-03 17:58:58 -0800

    Created/Exported: regex for rating

--- client/src/utils/regex.js
+++ client/src/utils/regex.js
@@ -4,4 +4,5 @@ export default {
   name: /^[a-zA-Z]{2}(([' -][a-zA-Z ])?[a-zA-Z]*)*$/,
   state: /^(?:A[KLRZ]|C[AOT]|D[CE]|FL|GA|HI|I[ADLN]|K[SY]|LA|M[ADEINOST]|N[CDEHJMVY]|O[HKR]|PA|RI|S[CD]|T[NX]|UT|V[AT]|W[AIVY])*$/,
   zipcode: /^[0-9]{5}$/,
+  rating: /^[1-5]{1}$/,
 };
