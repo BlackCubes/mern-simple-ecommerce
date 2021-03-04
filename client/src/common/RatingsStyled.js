e836924ccc8873b86285691a2477e1b660b83b36
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-04 05:52:38 -0800

    Added: space-evenly justify content for RatingsStyled

--- client/src/common/RatingsStyled.js
+++ client/src/common/RatingsStyled.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const RatingsStyled = styled.div`
   display: flex;
+  justify-content: space-evenly;
 `;
 
 export const RatingIconStyled = styled.div`
