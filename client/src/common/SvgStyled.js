be0a12f7ff8b85cea649fe39b71227c94a48998f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-04 05:51:31 -0800

    Increased: width and height for the svg

--- client/src/common/SvgStyled.js
+++ client/src/common/SvgStyled.js
@@ -3,6 +3,6 @@ import styled from 'styled-components';
 export default styled.svg.attrs({
   xmlns: 'http://www.w3.org/2000/svg',
 })`
-  width: 1.5rem;
-  height: 1.5rem;
+  width: 2.5rem;
+  height: 2.5rem;
 `;
