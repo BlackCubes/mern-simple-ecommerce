e1f3b7bd0f128a99c4c6cbaad5f9290bdcb453bf
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-03 21:58:23 -0800

    Imported/Exported: Ratings component

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -3,6 +3,15 @@ import CheckoutSidebar from './CheckoutSidebar';
 import CreditCard from './CreditCard';
 import Inputs from './Inputs';
 import Modal from './Modal';
+import Ratings from './Ratings';
 import Sidebar from './Sidebar';
 
-export { CategorySidebar, CheckoutSidebar, CreditCard, Inputs, Modal, Sidebar };
+export {
+  CategorySidebar,
+  CheckoutSidebar,
+  CreditCard,
+  Inputs,
+  Modal,
+  Ratings,
+  Sidebar,
+};
