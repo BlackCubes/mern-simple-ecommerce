799c89ed64ff67c0c58156c926bfa2d9306fc10f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-05 12:32:10 -0800

    Imported Modules: React, useState, and prop-types. Created: a login form field of an array of objects to display the type of inputs, and a LoginPage container component to handle/display the login page. PropTypes: FormContainerComponent in LoginPage. Export: LoginPage

---
+++ client/src/containers/LoginPage.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const loginFormFields = [
+  {
+    type: 'email',
+    name: 'email',
+    id: 'email',
+    placeholder: 'Email',
+    message: "Let's go!",
+    addlstyle: {
+      width: '100%',
+      float: 'left',
+      padding: '0 0.75rem',
+    },
+  },
+  {
+    type: 'password',
+    name: 'password',
+    id: 'password',
+    message: "Let's go!",
+    addlstyle: {
+      width: '100%',
+      float: 'left',
+      padding: '0 0.75rem',
+    },
+  },
+];
+
+const LoginPage = ({ FormContainerComponent }) => {
+  const [loginData, setLoginData] = useState({});
+
+  const onSubmission = (data) => setLoginData(data);
+
+  console.log(loginData);
+
+  return (
+    <FormContainerComponent
+      onSubmit={onSubmission}
+      formFields={loginFormFields}
+    />
+  );
+};
+
+LoginPage.propTypes = {
+  FormContainerComponent: PropTypes.elementType.isRequired,
+};
+
+export default LoginPage;
