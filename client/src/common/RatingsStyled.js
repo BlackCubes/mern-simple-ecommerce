ef9712e4ca45f032728fc95a329a52a9723aaaf6
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-06 11:26:52 -0800

    Styled: styled ratings-static class with left justify-content

--- client/src/common/RatingsStyled.js
+++ client/src/common/RatingsStyled.js
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 export const RatingsStyled = styled.div`
   display: flex;
   justify-content: space-evenly;
+
+  &.ratings-static {
+    justify-content: left;
+  }
 `;
 
 export const RatingIconStyled = styled.div`
