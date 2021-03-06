66e4678301d5e692f78bd149aaa2a74fb1208794
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-06 08:52:33 -0800

    Created: middleware to set the product id from params into the req.body

--- api/controllers/reviewController.js
+++ api/controllers/reviewController.js
@@ -1,5 +1,10 @@
 const factory = require('./handlerFactory');
 
+exports.setProductId = (req, res, next) => {
+  if (!req.body.productId) req.body.productId = req.params.productId;
+  next();
+};
+
 exports.getAllReviews = factory.getAll('Review');
 exports.getReview = factory.getOne('Review');
 exports.createReview = factory.createOne('Review');
