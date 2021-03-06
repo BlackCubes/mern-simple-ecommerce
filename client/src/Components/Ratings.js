0c5a018a90cc67700116df6a18f878c34376e8e4
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-05 18:52:57 -0800

    Moved: onRatingsChange to onSaveRating function

--- client/src/Components/Ratings.js
+++ client/src/Components/Ratings.js
@@ -47,9 +47,10 @@ const Ratings = ({ cursortype, onRatingsChange }) => {
 
   const onMouseEnter = (index) => setHoverRating(index);
   const onMouseLeave = () => setHoverRating(0);
-  const onSaveRating = (index) => setRating(index);
-
-  onRatingsChange(rating);
+  const onSaveRating = (index) => {
+    setRating(index);
+    onRatingsChange(index);
+  };
 
   return (
     <RatingsStyled>
