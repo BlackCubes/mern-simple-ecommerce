44c15f313a3959e682c4c776283f997c34bff590
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-07 09:48:31 -0800

    Removed: container imports, and the components property

--- client/src/utils/breadcrumbsRoutes.js
+++ client/src/utils/breadcrumbsRoutes.js
@@ -1,11 +1,8 @@
-import { ProductDetailsPage, ProductsPage } from '../containers';
-
 export default [
-  { path: '/products', name: 'Products', Component: ProductsPage },
+  { path: '/products', name: 'Products' },
   {
     path: '/products/category/:category',
     name: 'Category',
-    Component: ProductsPage,
   },
-  { path: '/products/:id', name: 'A Product', Component: ProductDetailsPage },
+  { path: '/products/:id', name: 'A Product' },
 ];
