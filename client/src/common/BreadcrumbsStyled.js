a69a0720508493f7d5d52a3e729da21d4c15e665
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-07 10:20:40 -0800

    Imported: React, prop-types module, and uuid module. Created/Exported: a breadcrumbs component. PropTypes: crumbs and LinkComponent

---
+++ client/src/common/BreadcrumbsStyled.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
+
+const Breadcrumbs = ({ crumbs, LinkComponent }) => {
+  if (crumbs.length <= 1) return null;
+
+  return (
+    <>
+      {crumbs.map(({ name, path }, key) =>
+        key + 1 === crumbs.length ? (
+          <div key={uuidv4()}>{name}</div>
+        ) : (
+          <LinkComponent key={uuidv4()} href={path}>
+            {name}
+          </LinkComponent>
+        )
+      )}
+    </>
+  );
+};
+
+Breadcrumbs.propTypes = {
+  crumbs: PropTypes.arrayOf(PropTypes.string).isRequired,
+  LinkComponent: PropTypes.elementType.isRequired,
+};
+
+export default Breadcrumbs;
