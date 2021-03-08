1c9c44ebbba40474d9d3a81687171791ffea56de
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-08 08:13:03 -0800

    Imported: useState from react, useHistory from react-router-dom module, headers utility function, and login and logout api functions. Created: user and its state, login async function which stores the user's login info with any token while setting a new token if successful, and a logout async function which checks if there are any current tokens while logging out the user and redirecting. Added To Provider: user, login, and logout

--- client/src/providers/AuthProvider.js
+++ client/src/providers/AuthProvider.js
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { AuthContext } from '../context';
 
-const AuthProvider = ({ children }) => (
-  <AuthContext.Provider>{children}</AuthContext.Provider>
-);
+import { headers, loginAPI, logoutAPI } from '../utils';
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const history = useHistory();
+
+  const login = async (data) => {
+    try {
+      const currentToken = localStorage.getItem('jwt') || null;
+      const { token, userData } = await loginAPI(data, headers(currentToken));
+
+      if (token && userData) {
+        setUser(userData);
+        localStorage.setItem('jwt', token);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      const currentToken = localStorage.getItem('jwt') || null;
+      const data = await logoutAPI(headers(currentToken));
+
+      if (data.status === 'success') history.push('/');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 AuthProvider.propTypes = {
   children: PropTypes.node.isRequired,
