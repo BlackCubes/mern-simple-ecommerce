2751d10f3290bb9fe782eb182c12b9a1a9f855fe
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-08 05:29:41 -0800

    Imported/Exported: contexts for authentication

--- client/src/context/index.js
+++ client/src/context/index.js
@@ -1,8 +1,11 @@
+import AuthContext, { useAuthContext } from './AuthContext';
 import CartContext, { useCartContext } from './CartContext';
 import CheckoutContext, { useCheckoutContext } from './CheckoutContext';
 import ProductContext, { useProductContext } from './ProductContext';
 
 export {
+  AuthContext,
+  useAuthContext,
   CartContext,
   useCartContext,
   CheckoutContext,
