faaaa07f05fb40a1cf4cb9a06f4cfe06e67a5fea
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-07 18:14:29 -0800

    Imported Module: styled-components. Created/Exported: custom styled components for the breadcrumbs

---
+++ client/src/common/BreadcrumbsStyled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const BreadcrumbsContainerStyled = styled.div`
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+`;
+
+export const BreadcrumbsStyled = styled.nav`
+  display: inline-block;
+  box-sizing: border-box;
+`;
+
+export const BreadcrumbsListStyled = styled.ol`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
+export const BreadcrumbsListItemStyled = styled.li`
+  display: inline;
+`;
