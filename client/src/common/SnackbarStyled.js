17e5d350e321e1e936bb1ee8ef9a0d3478b59784
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-12 06:48:27 -0800

    Imported: styled and keyframes from styled-components. Created: a fadeIn and fadeOut animation keyframes, and a snackbar custom styled component. Exported: snackbar custom styled component

---
+++ client/src/common/SnackbarStyled.js
@@ -0,0 +1,46 @@
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  from {
+    bottom: 0;
+    opacity: 0;
+  }
+  to {
+    bottom: 3rem;
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    bottom: 3rem;
+    opacity: 1;
+  }
+  to {
+    bottom: 0;
+    opacity: 0;
+  }
+`;
+
+export default styled.div`
+  visibility: hidden;
+  position: fixed;
+  min-width: 25rem;
+  margin-left: -12.5rem;
+  bottom: 3rem;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 9999;
+  color: white;
+  background-color: ${(props) => props.theme.colors[props.colortype]};
+  font-size: 1.6rem;
+  font-weight: 400;
+  text-align: center;
+  border-radius: 0.2rem;
+  padding: 1.6rem;
+
+  &.show {
+    visibility: visible;
+    animation: ${fadeIn} 0.5s, ${fadeOut} 0.5s 2.5s;
+  }
+`;
