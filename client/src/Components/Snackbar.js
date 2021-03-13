6fb14f7bc78a4d7c59d48cec51f136bf818f085a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-13 09:44:18 -0800

    Imported: useState from react. Added: isActive with its state and a setTimeout to render the className in order to display/not-display the snackbar

--- client/src/Components/Snackbar.js
+++ client/src/Components/Snackbar.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { SnackbarStyled } from '../common';
 
-const Snackbar = ({ children, color }) => (
-  <SnackbarStyled bgcolortype={color}>{children}</SnackbarStyled>
-);
+const Snackbar = ({ children, color }) => {
+  const [isActive, setIsActive] = useState(true);
+
+  setTimeout(() => {
+    setIsActive(false);
+  }, 5000);
+
+  return (
+    <SnackbarStyled className={isActive ? 'show' : ''} bgcolortype={color}>
+      {children}
+    </SnackbarStyled>
+  );
+};
 
 Snackbar.propTypes = {
   children: PropTypes.node.isRequired,
