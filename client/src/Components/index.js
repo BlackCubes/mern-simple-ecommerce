05cb5a128351143e4427de7d16f4fde7b0fee657
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-13 20:11:08 -0800

    Removed: the imports and exports from all sidebars

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -1,22 +1,16 @@
 import Breadcrumbs from './Breadcrumbs';
-import CategorySidebar from './CategorySidebar';
-import CheckoutSidebar from './CheckoutSidebar';
 import CreditCard from './CreditCard';
 import Inputs from './Inputs';
 import Modal from './Modal';
 import { RatingsInput, RatingsStatic } from './Ratings';
-import Sidebar from './Sidebar';
 import Snackbar from './Snackbar';
 
 export {
   Breadcrumbs,
-  CategorySidebar,
-  CheckoutSidebar,
   CreditCard,
   Inputs,
   Modal,
   RatingsInput,
   RatingsStatic,
-  Sidebar,
   Snackbar,
 };
