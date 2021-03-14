67fa5c9f31f6bf902beb21f2d0099705a48cce99
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-13 20:08:35 -0800

    Imported/Exported: all sidebars

---
+++ client/src/Components/Sidebar/index.js
@@ -0,0 +1,5 @@
+import CategorySidebar from './CategorySidebar';
+import CheckoutSidebar from './CheckoutSidebar';
+import Sidebar from './Sidebar';
+
+export { CategorySidebar, CheckoutSidebar, Sidebar };
