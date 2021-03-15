9d0f25e49a39f20ebf856b8838d522ffa9c9e6b7
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-14 19:48:52 -0700

    Removed: regex utility function and card-validator module. Imported: validateForm and validateStarRatings utility functions. Added: switch statement in handleChange function for credit cards. Refactored: the validations into functions by putting them into validate.js utility file

--- client/src/containers/FormContainer.js
+++ client/src/containers/FormContainer.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Card from 'react-credit-cards';
-import cardValidator from 'card-validator';
 
 import { Button, FormStyled, FormGroupStyled } from '../common';
 import { LabelStyled, InputMessageStyled } from '../common/Inputs';
@@ -14,7 +13,8 @@ import {
   formatExpDate,
   fieldInputErrors,
   fieldInputProperties,
-  regex,
+  validateForm,
+  validateStarRatings,
 } from '../utils';
 
 const FormContainer = ({
@@ -37,330 +37,30 @@ const FormContainer = ({
     let { value } = e.target;
 
     switch (name) {
-      case 'email':
-        if (!value.length)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Required.',
-          }));
-        else if (!regex.email.test(value))
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Please provide a valid email.',
-          }));
-        else setErrors((err) => ({ ...err, [name]: '' }));
-        break;
-
-      case 'password':
-        if (!value.length)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Required.',
-          }));
-        else if (value.length < 8)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Must be a minimum of 8 characters.',
-          }));
-        else if (value.length > 60)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Must be less than or equal to 60 characters.',
-          }));
-        else if (!regex.password.test(value))
-          setErrors((err) => ({
-            ...err,
-            [name]:
-              'Please use at least one number, one special character, and one capital letter between 8 to 60 characters.',
-          }));
-        else setErrors((err) => ({ ...err, [name]: '' }));
-        break;
-
-      case 'firstname':
-        if (!value.length)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Required.',
-          }));
-        else if (value.length < 2)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Must be a minimum of 2 characters.',
-          }));
-        else if (value.length > 20)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Must be less than or equal to 20 characters.',
-          }));
-        else if (!regex.name.test(value))
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Please use a valid first name.',
-          }));
-        else setErrors((err) => ({ ...err, [name]: '' }));
-        break;
-
-      case 'lastname':
-        if (!value.length)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Required.',
-          }));
-        else if (value.length < 2)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Must be a minimum of 2 characters.',
-          }));
-        else if (value.length > 40)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Must be less than or equal to 40 characters.',
-          }));
-        else if (!regex.name.test(value))
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Please use a valid last name.',
-          }));
-        else setErrors((err) => ({ ...err, [name]: '' }));
-        break;
-
-      case 'fullname':
-        if (!value.split(' ')[0])
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Required first name.',
-          }));
-        else if (!value.split(' ')[1])
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Required last name.',
-          }));
-        else if (value.length < 2)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Must be a minimum of 2 characters',
-          }));
-        else if (value.length > 70)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Must be less than or equal to 70 characters.',
-          }));
-        else if (!regex.name.test(value))
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Please use a valid fullname.',
-          }));
-        else setErrors((err) => ({ ...err, [name]: '' }));
-        break;
-
-      case 'address':
-        if (!value.length)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Required.',
-          }));
-        else if (value.length < 3)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Must be a minimum of 3 characters.',
-          }));
-        else if (value.length > 96)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Must be less than or equal to 96 characters.',
-          }));
-        else if (!regex.address.test(value))
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Please use a valid address.',
-          }));
-        else setErrors((err) => ({ ...err, [name]: '' }));
-        break;
-
-      case 'city':
-        if (!value.length)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Required.',
-          }));
-        else if (value.length < 3)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Must be a minimum of 3 characters.',
-          }));
-        else if (value.length > 50)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Must be less than or equal to 50 characters.',
-          }));
-        else if (!regex.city.test(value))
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Please use a valid city.',
-          }));
-        else setErrors((err) => ({ ...err, [name]: '' }));
-        break;
-
-      case 'state':
-        if (!value.length)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Required.',
-          }));
-        else if (value.length !== 2)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Must be 2 characters long.',
-          }));
-        else if (!regex.state.test(value))
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Please provide a valid US state.',
-          }));
-        else setErrors((err) => ({ ...err, [name]: '' }));
-        break;
-
-      case 'zipcode':
-        if (!value.length)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Required.',
-          }));
-        else if (value.length !== 5)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Must be 5 characters long.',
-          }));
-        else if (!regex.zipcode.test(value))
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Please provide a valid US ZIP code.',
-          }));
-        else setErrors((err) => ({ ...err, [name]: '' }));
-        break;
-
-      case 'review':
-        if (!value.length)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Required.',
-          }));
-        else if (value.length < 20)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Must be a minimum of 20 characters.',
-          }));
-        else if (value.length > 280)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Must be less than or equal to 280 characters.',
-          }));
-        else setErrors((err) => ({ ...err, [name]: '' }));
-        break;
-
-      case 'rating':
-        if (!value.length)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Required.',
-          }));
-        else if (parseInt(value, 10) < 1)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Must be a minimum rating of 1.',
-          }));
-        else if (parseInt(value, 10) > 5)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Must be a maximum rating of 5.',
-          }));
-        else if (!regex.rating.test(value))
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Please provide a valid rating between 1 and 5',
-          }));
-        else setErrors((err) => ({ ...err, [name]: '' }));
-        break;
-
       case 'number':
         value = formatCreditCardNumber(value);
-        if (!value.length)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Required.',
-          }));
-        else if (!cardValidator.number(value).isValid)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Please provide a valid number.',
-          }));
-        else setErrors((err) => ({ ...err, [name]: '' }));
         break;
-
       case 'expiry':
         value = formatExpDate(value);
-        if (!value.length)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Required.',
-          }));
-        else if (!cardValidator.expirationDate(value).isValid)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Please provide a valid exp. data.',
-          }));
-        else setErrors((err) => ({ ...err, [name]: '' }));
         break;
-
       case 'cvc':
         value = formatCvc(value);
-        if (!value.length)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Required.',
-          }));
-        else if (!cardValidator.cvv(value).isValid)
-          setErrors((err) => ({
-            ...err,
-            [name]: 'Please provide a valid cvv',
-          }));
-        else setErrors((err) => ({ ...err, [name]: '' }));
         break;
-
       default:
         break;
     }
+
+    validateForm(name, value, setErrors);
 
     setValues((val) => ({ ...val, [name]: value }));
   };
 
   const handleRatingsChange = (rating) => {
-    if (!rating)
-      setErrors((err) => ({
-        ...err,
-        rating: 'Required',
-      }));
-    else if (rating < 1)
-      setErrors((err) => ({
-        ...err,
-        rating: 'Must be a minimum rating of 1.',
-      }));
-    else if (rating > 5)
-      setErrors((err) => ({
-        ...err,
-        rating: 'Must be a maximum rating of 5.',
-      }));
-    else if (!regex.rating.test(rating))
-      setErrors((err) => ({
-        ...err,
-        rating: 'Please provide a valid rating between 1 and 5',
-      }));
-    else setErrors((err) => ({ ...err, rating: '' }));
-
+    validateStarRatings(rating, setErrors);
     setValues((val) => ({ ...val, rating }));
   };
 
-  const validateForm = (errorList) => {
+  const checkErrors = (errorList) => {
     let valid = true;
     Object.values(errorList).forEach((err) => {
       if (err.length) valid = false;
@@ -370,7 +70,7 @@ const FormContainer = ({
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (validateForm(errors)) {
+    if (checkErrors(errors)) {
       onSubmit(values);
       setValues({});
     }
