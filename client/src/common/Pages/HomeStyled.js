1e08f88af5dd3113ab0e9a4e12a71917927ee5ba
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-15 15:39:24 -0700

    Changed: custom styled componet names for HomeSection and HomeImage. Added: min-height to HomeSectionStyled. Created/Exported: 5 custom styled components targeting the the fake store api section

--- client/src/common/Pages/HomeStyled.js
+++ client/src/common/Pages/HomeStyled.js
@@ -6,12 +6,43 @@ export const HomePageSectionsContainerStyled = styled.div`
   margin-right: auto;
 `;
 
-export const HomePageStyled = styled.section`
+export const HomeSectionStyled = styled.section`
   width: 100%;
+  min-height: calc(100vh - 11rem);
   background-color: ${(props) => props.theme.colors.black_gray};
   text-align: center;
 `;
 
-export const HomePageImage = styled.div`
+export const HomeImageStyled = styled.div`
   width: 44rem;
+`;
+
+export const FakeStoreSectionStyled = styled.div`
+  position: relative;
+  width: 117rem;
+  background-color: ${(props) => props.theme.colors.vivid_pink};
+`;
+
+export const FakeStoreSplitHolderStyled = styled.div`
+  float: none;
+`;
+
+export const FakeStoreSplitColorStyled = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: ${(props) => props.theme.colors.white};
+  top: 0;
+  left: 40.5rem;
+  transform: translateX(-104%);
+`;
+
+export const FakeStoreImageStyled = styled.div`
+  width: 20rem;
+`;
+
+export const FakeStoreContent = styled.div`
+  float: right;
+  width: 60rem;
+  margin-top: 5rem;
 `;
