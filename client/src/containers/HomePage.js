a0ea18f738d14add744644afd4f92b844822021c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-15 17:30:27 -0700

    Imported: react, image custom styled component, all homepage custom styled components, and two typography custom styled components. Created/Exported: a component that renders the homepage

---
+++ client/src/containers/HomePage.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import { Image } from '../common';
+import {
+  HomePageSectionsContainerStyled,
+  HomeSectionStyled,
+  HomeImageStyled,
+  FakeStoreSectionStyled,
+  FakeStoreSplitHolderStyled,
+  FakeStoreSplitColorStyled,
+  FakeStoreImageStyled,
+  FakeStoreContent,
+} from '../common/Pages';
+import { HeadingPrimary, Paragraph } from '../common/Typography';
+
+const HomePage = () => (
+  <HomePageSectionsContainerStyled>
+    <HomeSectionStyled>
+      <HomeImageStyled>
+        <Image
+          rest={{ src: '/img/logo-large.png', alt: 'Large Company Logo' }}
+        />
+      </HomeImageStyled>
+
+      <HeadingPrimary>
+        Giving Power To The Users. Become The Person You Were Meant To Be. Own
+        That Power.
+      </HeadingPrimary>
+    </HomeSectionStyled>
+
+    <FakeStoreSectionStyled>
+      <FakeStoreSplitHolderStyled>
+        <FakeStoreSplitColorStyled />
+      </FakeStoreSplitHolderStyled>
+
+      <FakeStoreImageStyled>
+        <Image
+          rest={{
+            src: 'https://fakestoreapi.com/icons/intro.svg',
+            alt: 'Fake Store API',
+          }}
+        />
+      </FakeStoreImageStyled>
+
+      <FakeStoreContent>
+        <Paragraph>
+          With the power of the Fake Store API, you can own of of your favorite
+          products: electronics, jewelery, clothing and more
+        </Paragraph>
+      </FakeStoreContent>
+    </FakeStoreSectionStyled>
+  </HomePageSectionsContainerStyled>
+);
+
+export default HomePage;
