4f157ac9df193dd5892372f776d354205e4b4910
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-20 11:00:34 -0700

    Removed: useState from import

--- client/src/containers/HomePage.js
+++ client/src/containers/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { Button, Image } from '../common';
