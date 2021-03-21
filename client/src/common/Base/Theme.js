84e5eca75e158a95696c10c05f356bb87bb5fad8
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-21 10:19:46 -0700

    Added: a responsive property for responsive web design with different breakpoint values along with comments for clarity

--- client/src/common/Base/Theme.js
+++ client/src/common/Base/Theme.js
@@ -17,4 +17,40 @@ export default {
     xsmall: '1.1rem',
     small: '1.3rem',
   },
+  responsive: {
+    // MEDIA QUERY MANAGER
+    /*
+      Desktop-First
+
+      Let 1em = 16px
+
+      MAX-WIDTH:
+      74.9375em = 1199px
+      56.1875em = 899px
+      37.4375em = 599px
+      29.9375em = 479px
+      23.6875em = 379px
+
+      MAX-WIDTH AND MIN-WIDTH
+      61.9375em = 991px (max) AND 48em = 768px (min)
+      74.9375em = 1199px (max) AND 62em = 992px (min)
+    */
+
+    // Responsive Range: [1199px - 900px]
+    below_1199: '@media only screen and (max-width: 74.9375em)',
+    // Responsive Range: [899px - 600px]
+    below_899: '@media only screen and (max-width: 56.1875em)',
+    // Responsive Range: [599px - 480px]
+    below_599: '@media only screen and (max-width: 37.4375em)',
+    // Responsive Range: [479px - 380px]
+    below_479: '@media only screen and (max-width: 29.9375em)',
+    // Responsive Range: [379px - 0]
+    below_379: '@media only screen and (max-width: 23.6875em)',
+    // Responsive Range: [1199px - 992px] and portrait
+    between_1199_992:
+      '@media only screen and (max-width: 74.9375em) and (min-width: 62em) and (orientation: portrait)',
+    // Responsive Range: [991px - 768px] and portrait
+    between_991_768:
+      '@media only screen and (max-width: 61.9375em) and (min-width: 48em) and (orientation: portrait)',
+  },
 };
