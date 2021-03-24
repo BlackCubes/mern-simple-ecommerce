c07fe0c51d4ce3fdfda4cbf6298bc9758de2cbd3
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-24 06:28:29 -0700

    Added: classname to Sidebar to target it in the CSS

--- client/src/Components/Sidebar/CategorySidebar.js
+++ client/src/Components/Sidebar/CategorySidebar.js
@@ -37,7 +37,7 @@ const CategorySidebar = () => {
   }, [category]);
 
   return (
-    <SidebarStyled>
+    <SidebarStyled className="category-sidebar">
       <SidebarContainerStyled>
         <CategorySidebarStyled>
           <CategorySidebarHeaderStyled>
