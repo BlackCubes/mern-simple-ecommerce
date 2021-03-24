1509c04a21fe0f4ffb1bb9d6336dbba544fd7128
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-24 06:56:22 -0700

    Responsive Style (max-width 899): decreased font-size for HeadingSecondary typography

--- client/src/common/Typography/HeadingSecondary.js
+++ client/src/common/Typography/HeadingSecondary.js
@@ -5,6 +5,10 @@ import styled from 'styled-components';
 const HeadingSecondaryStyled = styled.h2`
   font-size: 3rem;
   font-weight: 600;
+
+  @media ${(props) => props.theme.responsive.below_899} {
+    font-size: 2.7rem;
+  }
 `;
 
 const HeadingSecondary = ({ children }) => (
