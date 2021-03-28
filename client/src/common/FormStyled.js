5d1d17cce3fb156a56006398abe0510a2fa19828
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-28 11:47:39 -0700

    Responsive Style (max-width 899px): decreased the width for tri-field-1 in FormGroup; and increased the width for tri-field-2 and tri-field-3 in FormGroup

--- client/src/common/FormStyled.js
+++ client/src/common/FormStyled.js
@@ -9,6 +9,21 @@ export const FormStyled = styled.form`
 export const FormGroupStyled = styled.div(
   {
     margin: '1.5rem 0',
+    '&.tri-field-1': {
+      '@media only screen and (max-width: 56.1875em)': {
+        width: 'calc(100% / 2.5)',
+      },
+    },
+    '&.tri-field-2': {
+      '@media only screen and (max-width: 56.1875em)': {
+        width: 'calc(100% / 4)',
+      },
+    },
+    '&.tri-field-3': {
+      '@media only screen and (max-width: 56.1875em)': {
+        width: 'calc(100% / 3)',
+      },
+    },
   },
   ({ addlstyle }) => addlstyle
 );
