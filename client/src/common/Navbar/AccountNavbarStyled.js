2769f13c9918ff370800b9a168d2c794bd37ba5a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-28 20:46:14 -0700

    Added position relative

--- client/src/common/Navbar/AccountNavbarStyled.js
+++ client/src/common/Navbar/AccountNavbarStyled.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const AccountNavbarStyled = styled.header`
+  position: relative;
   width: 100%;
   height: 11rem;
   background: ${(props) => props.theme.colors.black_gray};
