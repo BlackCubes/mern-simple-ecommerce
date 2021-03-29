9464d8072230ecb67d8925b92adcf0542040cd01
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-03-29 08:02:13 -0700

    Commented out, and set the height to initial for AccountFooterCreated

--- client/src/common/Footer/AccountFooterStyled.js
+++ client/src/common/Footer/AccountFooterStyled.js
@@ -4,17 +4,17 @@ export const AccountFooterStyled = styled.footer`
   position: absolute;
   font-family: inherit;
   width: 100%;
-  height: 16rem;
+  /* height: 16rem; */
   background-color: ${(props) => props.theme.colors.black_gray};
   bottom: 0;
 
-  @media ${(props) => props.theme.responsive.below_1199} {
+  /* @media ${(props) => props.theme.responsive.below_1199} {
     height: 12rem;
-  }
+  } */
 `;
 
 export const AccountFooterCreatedStyled = styled.div`
-  height: 10rem;
+  height: initial;
 `;
 
 export const AccountFooterCreatedContainerStyled = styled.div`
