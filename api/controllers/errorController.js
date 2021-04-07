4010ecdf696d5b5a68b670bcaf2edba0f3b7cc55
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-07 06:33:27 -0700

    Fixed Bug: changed minus symbol to an equals symbol (syntax error)

--- api/controllers/errorController.js
+++ api/controllers/errorController.js
@@ -60,7 +60,7 @@ module.exports = (err, req, res, next) => {
     error.message = err.message;
 
     if (error.name === 'CastError') error = handleCastErrorDB(error);
-    if (error.code === 11000) error - handleDuplicateFieldsDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'ValidationError')
       error = handleValidationErrorDB(error);
 
